15141ad6ccec1fb1cba9f3fccabd93cc620997fd
Kaio Felps <kaiofelipe2005@gmail.com>
2025-10-08 11:24:32 -0300

    chore: add `trimNullProperties` util

--- src/utils/trim-null-properties.ts
+++ src/utils/trim-null-properties.ts
@@ -4,8 +4,7 @@ export function trimNullProperties<T extends Record<PropertyKey, unknown>>(
   property: T,
 ): NullableToUndefined<T> {
   const record = Object.entries(property).reduce((obj, [key, value]) => {
-    if (value === null) return;
-    obj[key] = value;
+    if (value != null) obj[key] = value;
     return obj;
   }, {});
 
