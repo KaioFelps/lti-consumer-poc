d5b265a9e8d1e6240198a81ad50c27333d907110
Kaio Felps <kaiofelipe2005@gmail.com>
2025-10-09 12:39:16 -0300

    feat: setup dotenv expand & validator pipe to transform body json into dto classes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { config } from "dotenv";
+import { expand } from "dotenv-expand";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
+  expand(config());
+
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        excludeExtraneousValues: true,
+        strategy: "exposeAll",
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
