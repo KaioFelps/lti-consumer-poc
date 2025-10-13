c64f38d8f33b38d4fb66d4d3599fc33ebb67d910
Kaio Felps <kaiofelipe2005@gmail.com>
2025-10-13 16:00:03 -0300

    refactor(Validation Errors): rewrite entire validation errors class to handle a map of errors instead of an array

---
+++ src/core/validation/validation-error.spec.ts
@@ -0,0 +1,96 @@
+import { InvalidArgumentError } from "../errors/invalid-argument.error";
+import { ValidationErrors } from "./validation-errors";
+
+describe("ValidationErrors", () => {
+  it("should correctly append validation errors", () => {
+    const validationErrors = new ValidationErrors();
+
+    const error = new InvalidArgumentError(
+      "test:some:message:identifier",
+      "nonNestedArgument",
+    );
+    validationErrors.appendError(error);
+
+    expect(validationErrors.getErrors()).toMatchObject({
+      nonNestedArgument: [
+        {
+          errorMessageIdentifier: "test:some:message:identifier",
+          argumentName: "nonNestedArgument",
+          errorType: "InvalidArgumentError",
+        },
+      ],
+    });
+  });
+
+  it("should correctly handle multiple validation errors under a same argument/field", () => {
+    const validationErrors = new ValidationErrors();
+    for (const id of ["test:identifier", "test:another-identifier"]) {
+      validationErrors.appendError(
+        new InvalidArgumentError(id, "nonNestedArgument"),
+      );
+    }
+
+    expect(validationErrors.getErrors()).toMatchObject({
+      nonNestedArgument: [
+        {
+          errorMessageIdentifier: "test:identifier",
+          argumentName: "nonNestedArgument",
+          errorType: "InvalidArgumentError",
+        },
+        {
+          errorMessageIdentifier: "test:another-identifier",
+
+          argumentName: "nonNestedArgument",
+          errorType: "InvalidArgumentError",
+        },
+      ],
+    });
+  });
+
+  it("should handle validation of nested arguments from some schema", () => {
+    const validationErrors = new ValidationErrors();
+
+    for (const [id, arg] of [
+      ["test:identifier-1", "some.nested.arg"],
+      ["test:identifier-2", "some.nested.arg"],
+      ["test:identifier-1", "some.arg"],
+      ["test:identifier-1", "some.nested.evenDeeper.arg"],
+    ])
+      validationErrors.appendError(new InvalidArgumentError(id, arg));
+
+    expect(validationErrors.getErrors()).toMatchObject({
+      some: {
+        nested: {
+          arg: [
+            {
+              errorMessageIdentifier: "test:identifier-1",
+              argumentName: "some.nested.arg",
+              errorType: "InvalidArgumentError",
+            },
+            {
+              errorMessageIdentifier: "test:identifier-2",
+              argumentName: "some.nested.arg",
+              errorType: "InvalidArgumentError",
+            },
+          ],
+          evenDeeper: {
+            arg: [
+              {
+                errorMessageIdentifier: "test:identifier-1",
+                argumentName: "some.nested.evenDeeper.arg",
+                errorType: "InvalidArgumentError",
+              },
+            ],
+          },
+        },
+        arg: [
+          {
+            errorMessageIdentifier: "test:identifier-1",
+            argumentName: "some.arg",
+            errorType: "InvalidArgumentError",
+          },
+        ],
+      },
+    });
+  });
+});
--- src/core/validation/validation-errors.ts
+++ src/core/validation/validation-errors.ts
@@ -1,27 +1,63 @@
 import { InvalidArgumentError } from "../errors/invalid-argument.error";
 
-export type ValidationError =
-  | InvalidArgumentError
-  | {
-      [key: string]: ValidationError;
-    };
+export type ValidationError = InvalidArgumentError;
+
+export type ValidationErrorsMap = Record<
+  string,
+  ValidationError[] | { [key: string]: ValidationErrorsMap }
+>;
 
 export class ValidationErrors {
-  private errors: ValidationError[];
+  private errors: ValidationErrorsMap;
 
   public constructor() {
-    this.errors = [];
+    this.errors = {};
+  }
+
+  public getErrors() {
+    return this.errors;
   }
 
   public appendError(error: ValidationError) {
-    this.errors.push(error);
+    if (!this.ensureErrorRequiredFields(error)) return;
+    const path = error.argumentName.split(".");
+    this.recursivelyInsertError(error, path);
+  }
+
+  private recursivelyInsertError(
+    error: Required<ValidationError>,
+    path: string[],
+  ) {
+    let errorsWindow = this.errors;
+    const lastSegment = path[path.length - 1];
+    path = path.slice(0, path.length - 1);
+
+    for (const segment of path) {
+      if (!errorsWindow[segment]) errorsWindow[segment] = {};
+      errorsWindow = errorsWindow[segment] as ValidationErrorsMap;
+    }
+
+    if (!errorsWindow[lastSegment]) errorsWindow[lastSegment] = [];
+
+    (errorsWindow[lastSegment] as ValidationError[]).push(error);
   }
 
   public isEmpty() {
-    return this.errors.length === 0;
+    return Object.keys(this.errors).length === 0;
   }
 
   public merge(valiationErrors: ValidationErrors) {
-    this.errors = [...this.errors, ...valiationErrors.errors];
+    this.errors = { ...this.errors, ...valiationErrors.errors };
+  }
+
+  private ensureErrorRequiredFields(
+    error: InvalidArgumentError,
+  ): error is Required<InvalidArgumentError> {
+    if (typeof error.argumentName === "undefined")
+      throw new Error(
+        "Every validation error's `InvalidArgumentError` instance needs to have a defined `argumentName` value.",
+      );
+
+    return true;
   }
 }
