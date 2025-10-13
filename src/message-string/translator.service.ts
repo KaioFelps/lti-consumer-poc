de71210802fd07040f6b0aaa0cc285c4bf3a3616
Kaio Felps <kaiofelipe2005@gmail.com>
2025-10-13 16:00:51 -0300

    change(`TranslatorService`): make translator be instantiated per request, make it asynchronous and identify user's prefered language by checking a cookie's value

--- src/message-string/translator.service.ts
+++ src/message-string/translator.service.ts
@@ -1,14 +1,29 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable, Scope } from "@nestjs/common";
+import { REQUEST } from "@nestjs/core";
+import { HttpRequest } from "@/lib";
 import { MessageStringFormatterArg } from "./internal";
 import { translate } from "./internal/translate";
-import { ptBR } from "./internal/translations/pt-BR";
 
-@Injectable()
+@Injectable({ scope: Scope.REQUEST })
 export class TranslatorService {
-  public translate(
+  public constructor(@Inject(REQUEST) private readonly request: HttpRequest) {}
+
+  public async translate(
     identifier: string,
     args?: MessageStringFormatterArg,
-  ): string {
-    return translate(identifier, args, ptBR, "pt-BR");
+  ): Promise<string> {
+    const language = this.request.cookies.lti_consumer_poc_language ?? "pt-BR";
+    const strings = await this.getStringsFromLanguageTag(language);
+
+    if (!strings) return identifier;
+
+    return translate(identifier, args, strings, language);
+  }
+
+  private async getStringsFromLanguageTag(tag: string) {
+    switch (tag) {
+      case "pt-BR":
+        return (await import("./internal/translations/pt-BR")).ptBR;
+    }
   }
 }
