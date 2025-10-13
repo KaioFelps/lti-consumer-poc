5c8cf1c9cac13cb5f2e078ca23650299f21d89ce
Kaio Felps <kaiofelipe2005@gmail.com>
2025-10-13 15:55:09 -0300

    build: add cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from "@nestjs/core";
+import cookieParser from "cookie-parser";
 import { config } from "dotenv";
 import { expand } from "dotenv-expand";
 import { AppModule } from "./app.module";
@@ -8,6 +9,7 @@ async function bootstrap() {
   expand(config());
 
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.useGlobalPipes(new CoreValidationPipe());
 
   await app.listen(process.env.PORT ?? 3000);
