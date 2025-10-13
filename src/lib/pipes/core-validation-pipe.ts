7f1946290171ecc5dc8eb7107de83c8f41885527
Kaio Felps <kaiofelipe2005@gmail.com>
2025-10-13 10:49:39 -0300

    build: replace nestjs class validator with custom `CoreValidationPipe`

---
+++ src/lib/pipes/core-validation-pipe.ts
@@ -0,0 +1,51 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from "@nestjs/common";
+import { plainToInstance } from "class-transformer";
+import { either } from "fp-ts";
+import type { DTO } from "@/core/interfaces/dto";
+
+/**
+ * Validates the body parameter (decorated with `@Body()`).
+ * The body value must be an instance of some class which
+ * implements the `DTO` interface.
+ */
+export class CoreValidationPipe implements PipeTransform {
+  transform(value: unknown, metadata: ArgumentMetadata) {
+    if (metadata.type !== "body") return value;
+
+    if (!metadata.metatype)
+      throw new Error(
+        "The body value needs to have an explicit type. Ensure it has a proper " +
+          "type and that it is a concrete class instead of a type or interface.",
+      );
+
+    if (
+      !("validate" in metadata.metatype.prototype) ||
+      typeof metadata.metatype.prototype.validate !== "function"
+    ) {
+      // Help developer find out whether the value type is a class
+      // which implements the DTO interface.
+      throw new Error(
+        metadata.metatype
+          ? `Class ${metadata.metatype.name} does not implement DTO interface.`
+          : "The body value has not received any class prototype to be validated. Either type the body parameter or remove `CoreValidationPipe` from the pipeline.",
+      );
+    }
+
+    const valueAsInstanceOfADto: DTO = plainToInstance(
+      metadata.metatype,
+      value,
+      {
+        excludeExtraneousValues: true,
+        strategy: "excludeAll",
+      },
+    );
+
+    const isValid = valueAsInstanceOfADto.validate();
+    if (either.isLeft(isValid)) throw new BadRequestException(isValid.left);
+    return valueAsInstanceOfADto;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,14 @@
-import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { config } from "dotenv";
 import { expand } from "dotenv-expand";
 import { AppModule } from "./app.module";
+import { CoreValidationPipe } from "./lib/pipes/core-validation-pipe";
 
 async function bootstrap() {
   expand(config());
 
   const app = await NestFactory.create(AppModule);
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      transformOptions: {
-        excludeExtraneousValues: true,
-        strategy: "exposeAll",
-      },
-    }),
-  );
+  app.useGlobalPipes(new CoreValidationPipe());
 
   await app.listen(process.env.PORT ?? 3000);
 }
