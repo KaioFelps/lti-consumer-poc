800cdb8d534b1515fd824f8ebc7b52f115eeea15
Kaio Felps <kaiofelipe2005@gmail.com>
2025-10-15 10:12:18 -0300

    feat: add `UsersRepository` interface + related message strings

--- src/identity/strings/pt-BR.ts
+++ src/identity/strings/pt-BR.ts
@@ -12,6 +12,20 @@ export const identityMessageStringsPTBR: MessageStringTranslationMap = {
   },
 
   "identity:person:cpf:invalid-cpf": "O CPF fornecido é inválido.",
+
+  "identity:users-repository:find-user-by-username:resource-not-found": (
+    { username }: { username: string | undefined },
+    identifier,
+  ) => {
+    if (!username) {
+      console.warn(
+        `The message string "${identifier}" expected ` +
+          `an \`args.username\` property.`,
+      );
+    }
+
+    return `Não foi encontrado nenhum usuário com o username "${username}".`;
+  },
 };
 
 Object.freeze(identityMessageStringsPTBR);
---
+++ src/identity/user/users.repository.ts
@@ -0,0 +1,23 @@
+import { Either } from "fp-ts/lib/Either";
+import { IrrecoverableError } from "@/core/errors/irrecoverable-error";
+import { ResourceNotFoundError } from "@/core/errors/resource-not-found.error";
+import { User } from "./user.entity";
+
+export abstract class UsersRepository {
+  /**
+   * Message strings identifiers for possible errors originated from methods within
+   * this repository. Implementations inherit these messages and therefore does not
+   * need to write hard-coded strings identifiers, avoiding spelling errors or
+   * inconsistency.
+   */
+  protected static messages = {
+    findUserByUsername: {
+      resourceNotFound:
+        "identity:users-repository:find-user-by-username:resource-not-found",
+    },
+  } as const;
+
+  abstract findUserByUsername(
+    username: string,
+  ): Promise<Either<ResourceNotFoundError | IrrecoverableError, User>>;
+}
