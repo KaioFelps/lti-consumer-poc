240fa6d754fbcb05acf586dea817ae43276eae64
Kaio Felps <kaiofelipe2005@gmail.com>
2025-10-16 11:58:18 -0300

    change(`ErrorBase`): holds optional message params object

--- src/core/errors/error-base.ts
+++ src/core/errors/error-base.ts
@@ -1,16 +1,32 @@
 import { ClassProperties } from "common/src/types/class-properties";
+import { MessageStringFormatterArg } from "@/message-string/internal";
 
 export type ErrorClassProperties<Class> = Omit<
   ClassProperties<Class>,
   "errorType"
 >;
 
+/**
+ * `ErrorBase` is the base class for every core-level error. It models
+ * the minimum data every error should hold in order to be meaningful.
+ *
+ * Errors may add new properties as needed. Some errors might need only to
+ * enforce a message params exists with valid data. They can do this in
+ * several ways, such as making `messageParams` required (using
+ * TypeScript's `Required` type) or adding extra properties and adding
+ * them to message params inside its constructor.
+ */
 export abstract class ErrorBase {
   /**
-   * A namespaced error code that may be used by a client to find and display a
+   * A domain-namespaced error code that may be used by a client to find and display a
    * message string that explains this error.
    */
   public readonly errorMessageIdentifier: string;
+  /**
+   * Optional parameters that the message-strings module may use when formatting
+   * the message identified by `errorMessageIdentifier`.
+   */
+  public readonly messageParams?: MessageStringFormatterArg;
   /**
    * A code that indicates the type of this error. May be used by
    * a client to determine the appropriate way to handle this error.
--- src/core/errors/resource-not-found.error.ts
+++ src/core/errors/resource-not-found.error.ts
@@ -1,15 +1,9 @@
 import { ErrorBase, ErrorClassProperties } from "./error-base";
 
 export class ResourceNotFoundError extends ErrorBase {
-  /**
-   * Custom parameters that the message string may use to format
-   * the message.
-   */
-  public messageParams: Record<string, string | number | boolean> = {};
-
-  public constructor(properties: ErrorClassProperties<ResourceNotFoundError>) {
-    const { messageParams, ...superProperties } = properties;
-    super(superProperties, ResourceNotFoundError.name);
-    this.messageParams = messageParams;
+  public constructor(
+    properties: Required<ErrorClassProperties<ResourceNotFoundError>>,
+  ) {
+    super(properties, ResourceNotFoundError.name);
   }
 }
