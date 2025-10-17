c7a3d594bad77c58ef3b852a310f4164ac753599
Kaio Felps <kaiofelipe2005@gmail.com>
2025-10-17 00:24:33 -0300

    feat: add `PersonPresenter`

---
+++ src/external/presenters/entities/person.presenter.ts
@@ -0,0 +1,31 @@
+import { Person } from "@/identity/person/person.entity";
+
+type PresentedPerson = {
+  id: string;
+  systemRole: string;
+  username: string;
+  email: string;
+  firstName: string;
+  surname: string;
+  cpf: string;
+  birthDate: Date;
+  gender: string | null;
+  profilePictureUrl: string | null;
+};
+
+export class PersonPresenter {
+  public static present(entity: Person): PresentedPerson {
+    return {
+      birthDate: entity.getBirthDate(),
+      cpf: entity.getCpf().toString(),
+      email: entity.getEmail(),
+      firstName: entity.getFirstName(),
+      gender: entity.getGender()?.toString() ?? null,
+      id: entity.getUser().getId().toString(),
+      profilePictureUrl: entity.getUser().getProfilePictureUrl(),
+      surname: entity.getSurname(),
+      systemRole: entity.getUser().getSystemRole().toString(),
+      username: entity.getUser().getUsername(),
+    };
+  }
+}
