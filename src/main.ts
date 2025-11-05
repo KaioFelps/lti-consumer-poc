65fc6122e3a8b4d60c85f0c86be7ee56a8be0f9c
Kaio Felps <kaiofelipe2005@gmail.com>
2025-11-05 09:29:10 -0300

    feat: setup ejs

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
+import { join } from "node:path";
 import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
 import cookieParser from "cookie-parser";
 import { config } from "dotenv";
 import { expand } from "dotenv-expand";
@@ -8,10 +10,16 @@ import { CoreValidationPipe } from "./lib/pipes/core-validation-pipe";
 async function bootstrap() {
   expand(config());
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.use(cookieParser());
   app.useGlobalPipes(new CoreValidationPipe());
 
+  app.useStaticAssets(join(__dirname, "../..", "public"));
+  app.setBaseViewsDir(join(__dirname, "../..", "views"));
+  app.setViewEngine("ejs");
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
