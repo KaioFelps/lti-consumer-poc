cc355b57778668a5b7c2490f962ad2d89e693c07
Kaio Felps <kaiofelipe2005@gmail.com>
2025-11-27 11:31:31 -0300

    docs(ltilib/`MessagePlacement`): add source to docstring

--- ltilib/src/registration/enums/message-placement.ts
+++ ltilib/src/registration/enums/message-placement.ts
@@ -4,6 +4,8 @@
  *
  * When declared by a LTI Consumer, it's a way of informing LTI Tools the contexts
  * that this consumer supports to place the message.
+ *
+ * See: https://www.imsglobal.org/spec/lti-dr/v1p0#placements-for-message-type-ltideeplinkingrequest
  */
 export enum MessagePlacement {
   ContentArea = "ContentArea",
