1342f7fb76d229919ed341313d432b2f8a14b92d
Kaio Felps <kaiofelipe2005@gmail.com>
2025-11-27 12:27:33 -0300

    fix(`Redis` client): register it in datastore module

--- src/external/data-store/datastore.module.ts
+++ src/external/data-store/datastore.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { EnvironmentVars } from "@/config/environment-vars";
 import { DrizzleClient } from "./drizzle/client";
 import { RepositoriesModule } from "./drizzle/repositories/repositories.module";
+import { Redis } from "./redis/client";
 
 @Module({
   providers: [
@@ -10,8 +11,9 @@ import { RepositoriesModule } from "./drizzle/repositories/repositories.module";
       useFactory: DrizzleClient.create,
       inject: [EnvironmentVars],
     },
+    Redis,
   ],
-  exports: [DrizzleClient],
+  exports: [DrizzleClient, Redis],
   imports: [RepositoriesModule],
 })
 export class DataStoreModule {}
