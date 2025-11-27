364bea9188a2559d9124dad312b3c2edefbff9de
Kaio Felps <kaiofelipe2005@gmail.com>
2025-11-27 12:33:36 -0300

    chore(oidc): add `OIDCServerErrorException` for OAuth2 specified server error responses

---
+++ src/lib/exceptions/oidc/exception.ts
@@ -0,0 +1,19 @@
+import { HttpException, HttpStatus } from "@nestjs/common";
+
+/**
+ * Custom throwable exception to be presented as a OAuth2
+ * valid error, since oidc-provider does not expose such error from its
+ * `errors` namespace.
+ */
+export class OIDCServerErrorException extends HttpException {
+  public constructor() {
+    super(
+      {
+        error: "server_error",
+        error_description:
+          "Something went wrong in our server. Try again later.",
+      },
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+}
