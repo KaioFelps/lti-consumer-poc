0cf4ae71e3bdbe67c7334814e87d163a6e4db022
Kaio Felps <kaiofelipe2005@gmail.com>
2025-11-27 11:35:22 -0300

    feat(lti): add `LtiTool` aggregation to deal with ltilib `ToolRecord` and oidc-provider `ClientMetadata`

---
+++ src/lti/lti-tool-config-schemas.ts
@@ -0,0 +1,114 @@
+import z from "zod";
+import {
+  AnyLtiRole,
+  InstitutionRole,
+  MembershipRole,
+  SystemRole,
+} from "$/claims";
+import { MessageType } from "$/claims/serialization";
+import {
+  GrantType,
+  LTI_TOOL_CONFIGURATION_KEY,
+} from "$/registration/dynamic/tool-configuration";
+import { MessagePlacement } from "$/registration/enums/message-placement";
+
+/**
+ * Describes the values and its constraints specified by [LTI Dynamic Registration] specifications.
+ *
+ * [LTI Dynamic Registration]: https://www.imsglobal.org/spec/lti-dr/v1p0#lti-configuration-0
+ */
+export const ltiToolConfigurationSchema = z.object({
+  domain: z.string(),
+  secondary_domains: z.array(z.string()).optional(),
+  deployment_id: z.string().optional(),
+  target_link_uri: z.url(),
+  description: z.string().optional(),
+  claims: z.array(z.string()),
+  custom_parameters: z.record(z.string(), z.string()).optional(),
+  messages: z.array(
+    z.object({
+      type: z.enum([
+        MessageType.deepLinking,
+        MessageType.resourceLink,
+      ] satisfies MessageType[]),
+      target_link_uri: z.url().optional(),
+      label: z.string().optional(),
+      icon_uri: z.url().optional(),
+      placements: z
+        .array(
+          z.enum([
+            MessagePlacement.ContentArea,
+            MessagePlacement.RichTextEditor,
+          ] satisfies MessagePlacement[]),
+        )
+        .optional(),
+      roles: z
+        .array(
+          z.enum([
+            ...[
+              InstitutionRole.Administrator,
+              InstitutionRole.Faculty,
+              InstitutionRole.Guest,
+              InstitutionRole.None,
+              InstitutionRole.Other,
+              InstitutionRole.Staff,
+              InstitutionRole.Student,
+              InstitutionRole.Alumni,
+              InstitutionRole.Instructor,
+              InstitutionRole.Learner,
+              InstitutionRole.Member,
+              InstitutionRole.Mentor,
+              InstitutionRole.Observer,
+              InstitutionRole.ProspectiveStudent,
+            ],
+            ...[
+              MembershipRole.Administrator,
+              MembershipRole.ContentDeveloper,
+              MembershipRole.Instructor,
+              MembershipRole.Learner,
+              MembershipRole.Mentor,
+              MembershipRole.Manager,
+              MembershipRole.Member,
+              MembershipRole.Officer,
+            ],
+            ...[
+              SystemRole.Administrator,
+              SystemRole.None,
+              SystemRole.AccountAdmin,
+              SystemRole.Creator,
+              SystemRole.SysAdmin,
+              SystemRole.SysSupport,
+              SystemRole.User,
+            ],
+          ] satisfies AnyLtiRole[]),
+        )
+        .optional(),
+      custom_parameters: z.record(z.string(), z.string()).optional(),
+    }),
+  ),
+});
+
+/**
+ * This object describes the expected tool configuration sent by some
+ * tool during dynamic register.
+ */
+export const draftLtiDynamicToolConfigurationSchema = z.object({
+  client_id: z.string().optional(),
+  application_type: z.enum(["web"]),
+  grant_types: z
+    .array(z.enum(["client_credentials", "implicit"] satisfies GrantType[]))
+    .min(2),
+  response_types: z.array(z.enum(["id_token"])).min(1),
+  redirect_uris: z.array(z.url()).min(1),
+  initiate_login_uri: z.url(),
+  client_name: z.string(),
+  jwks_uri: z.url(),
+  logo_uri: z.url().optional(),
+  token_endpoint_auth_method: z.enum(["private_key_jwt"]),
+  contacts: z.array(z.email()).optional(),
+  client_uri: z.url().optional(),
+  tos_uri: z.url().optional(),
+  policy_uri: z.url().optional(),
+  scope: z.string(),
+  [LTI_TOOL_CONFIGURATION_KEY]: ltiToolConfigurationSchema,
+});
---
+++ src/lti/lti-tool.ts
@@ -0,0 +1,94 @@
+import { either } from "fp-ts";
+import { pipe } from "fp-ts/lib/function";
+import { nanoid } from "nanoid";
+import { ClientMetadata } from "oidc-provider";
+import { ZodError } from "zod";
+import { InvalidArgumentError } from "@/core/errors/invalid-argument.error";
+import { ValidationErrors } from "@/core/validation/validation-errors";
+import { mapZodErrorsToCoreValidationErrors } from "@/lib/zod/map-zod-errors-to-core-validation-error";
+import {
+  Contact,
+  LTI_TOOL_CONFIGURATION_KEY,
+} from "$/registration/dynamic/tool-configuration";
+import { ToolRecord } from "$/registration/tool-record";
+import { ToolSupportedMessage } from "$/registration/tool-supported-message";
+import { draftLtiDynamicToolConfigurationSchema } from "./lti-tool-config-schemas";
+
+export class LtiTool {
+  public constructor(public record: ToolRecord) {}
+
+  public static tryCreateFromClientMetadata(metadata: object) {
+    return pipe(
+      either.tryCatch(
+        () => draftLtiDynamicToolConfigurationSchema.parse(metadata),
+        (err) => mapZodErrorsToCoreValidationErrors(err as ZodError),
+      ),
+      either.map((metadata) => {
+        const ltiConfig = metadata[LTI_TOOL_CONFIGURATION_KEY];
+        const ltiTool = ToolRecord.create({
+          id: metadata.client_id ?? nanoid(),
+          applicationType: metadata.application_type,
+          contacts: metadata.contacts as Contact[],
+          grantTypes: metadata.grant_types,
+          name: metadata.client_name,
+          responseTypes: metadata.response_types,
+          scope: metadata.scope,
+          tokenEndpointAuthMethod: metadata.token_endpoint_auth_method,
+          uris: {
+            initiate: metadata.initiate_login_uri,
+            jwks: metadata.jwks_uri,
+            redirect: metadata.redirect_uris,
+            homePage: metadata.client_uri,
+            logo: metadata.logo_uri,
+            policy: metadata.policy_uri,
+            tos: metadata.tos_uri,
+          },
+          ltiConfiguration: {
+            claims: ltiConfig.claims,
+            domain: ltiConfig.domain,
+            messages: ltiConfig.messages.map(
+              (message) =>
+                new ToolSupportedMessage({
+                  type: message.type,
+                  customParameters: message.custom_parameters,
+                  iconUri: message.icon_uri,
+                  label: message.label,
+                  placements: message.placements,
+                  roles: message.roles,
+                  targetLinkUri: message.target_link_uri,
+                }),
+            ),
+            targetLinkUri: ltiConfig.target_link_uri,
+            customParameters: ltiConfig.custom_parameters,
+            description: ltiConfig.description,
+            deploymentsIds: [],
+          },
+        });
+        return pipe(
+          ltiTool,
+          either.map((record) => new LtiTool(record)),
+          either.mapLeft((err) => {
+            const validationErrors = new ValidationErrors();
+
+            Object.entries(err.errors).forEach(([key, _]) => {
+              const invalidArgumentError = new InvalidArgumentError({
+                errorMessageIdentifier: `lti:tool:create:errors:ltilib-${key}`,
+                argumentName: key,
+              });
+              validationErrors.appendError(invalidArgumentError);
+            });
+
+            return validationErrors;
+          }),
+        );
+      }),
+      either.flattenW,
+    );
+  }
+
+  public intoOIDCClient(): ClientMetadata {
+    const ltiClaims = this.record.intoLtiClaim();
+    const { [LTI_TOOL_CONFIGURATION_KEY]: _, ...claims } = ltiClaims;
+    return claims;
+  }
+}
