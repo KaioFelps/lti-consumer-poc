88eb5f747eceb07c899733d542caf35befb3253a
Kaio Felps <kaiofelipe2005@gmail.com>
2025-11-27 12:31:39 -0300

    change(oidc clients repository): add `upserClient` method

--- src/external/data-store/drizzle/repositories/oidc-clients.repository.ts
+++ src/external/data-store/drizzle/repositories/oidc-clients.repository.ts
@@ -1,10 +1,21 @@
 import { Injectable } from "@nestjs/common";
+import { either } from "fp-ts";
+import { Either } from "fp-ts/lib/Either";
 import { ClientMetadata } from "oidc-provider";
+import { IrrecoverableError } from "@/core/errors/irrecoverable-error";
+import { OIDCClient } from "@/oidc/client";
 import { OIDCClientsRepository } from "@/oidc/repositories/clients.repository";
 
 @Injectable()
 export class DrizzleOIDCClientsRepository extends OIDCClientsRepository {
-  public async getClients(): Promise<ClientMetadata[]> {
-    return [];
+  public async getClients(): Promise<Either<IrrecoverableError, OIDCClient[]>> {
+    console.warn("get clients not implemented");
+    return either.right([]);
+  }
+
+  public async upsertClient(
+    _client: OIDCClient,
+  ): Promise<Either<IrrecoverableError, ClientMetadata>> {
+    throw new Error("upsertClient not implemented.");
   }
 }
--- src/oidc/repositories/clients.repository.ts
+++ src/oidc/repositories/clients.repository.ts
@@ -1,5 +1,13 @@
+import { Either } from "fp-ts/lib/Either";
 import { ClientMetadata } from "oidc-provider";
+import { IrrecoverableError } from "@/core/errors/irrecoverable-error";
+import { OIDCClient } from "../client";
 
 export abstract class OIDCClientsRepository {
-  public abstract getClients(): Promise<ClientMetadata[]>;
+  public abstract getClients(): Promise<
+    Either<IrrecoverableError, OIDCClient[]>
+  >;
+  public abstract upsertClient(
+    client: OIDCClient,
+  ): Promise<Either<IrrecoverableError, ClientMetadata>>;
 }
