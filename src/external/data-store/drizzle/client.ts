dff8c9f513a31a6069346a5afb948434e3bc443c
Kaio Felps <kaiofelipe2005@gmail.com>
2025-11-28 02:13:39 -0300

    fix(drizzle): add schema to provide intellisense to client

--- src/external/data-store/drizzle/client.ts
+++ src/external/data-store/drizzle/client.ts
@@ -1,11 +1,12 @@
 import { Injectable } from "@nestjs/common";
+import * as schema from "drizzle/schema";
 import { drizzle, type NodePgDatabase } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import type { EnvironmentVars } from "@/config/environment-vars";
 
 @Injectable()
 export class DrizzleClient {
-  private constructor(private client: NodePgDatabase) {}
+  private constructor(private client: NodePgDatabase<typeof schema>) {}
 
   public static create(envVars: EnvironmentVars) {
     const pool = new Pool({
@@ -18,6 +19,7 @@ export class DrizzleClient {
 
     const db = drizzle({
       client: pool,
+      schema,
     });
 
     return new DrizzleClient(db);
