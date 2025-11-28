cb51f8526892139cef6d2f13465daeab64cd6ed5
Kaio Felps <kaiofelipe2005@gmail.com>
2025-11-28 02:13:06 -0300

    build(db): add lti and oauth tables & add drizzle relations

---
+++ drizzle/0004_add-lti-and-oauth-client-tables-and-add-tables-relations.sql
@@ -0,0 +1,72 @@
+CREATE TYPE "public"."oauth_application_type" AS ENUM('web', 'native');--> statement-breakpoint
+CREATE TABLE "lti_deployments" (
+	"client_id" varchar(64) NOT NULL,
+	"id" uuid NOT NULL,
+	CONSTRAINT "lti_deployments_client_id_id_pk" PRIMARY KEY("client_id","id")
+);
+--> statement-breakpoint
+CREATE TABLE "lti_tool_supported_message_role" (
+	"client_id" varchar(64) NOT NULL,
+	"msg_type" varchar NOT NULL,
+	"role" varchar NOT NULL,
+	CONSTRAINT "lti_tool_supported_message_role_client_id_msg_type_role_pk" PRIMARY KEY("client_id","msg_type","role")
+);
+--> statement-breakpoint
+CREATE TABLE "lti_tool_supported_messages" (
+	"client_id" varchar(64) NOT NULL,
+	"type" varchar NOT NULL,
+	"target_link_uri" varchar,
+	"label" varchar,
+	"icon_uri" varchar,
+	"placements" varchar,
+	"custom_parameters" jsonb,
+	CONSTRAINT "lti_tool_supported_messages_client_id_type_pk" PRIMARY KEY("client_id","type")
+);
+--> statement-breakpoint
+CREATE TABLE "lti_tools" (
+	"id" varchar(64) PRIMARY KEY NOT NULL,
+	"description" varchar,
+	"domain" varchar NOT NULL,
+	"custom_parameters" jsonb,
+	"response_types" varchar NOT NULL,
+	"initiate_uri" varchar NOT NULL,
+	"target_link_uri" varchar NOT NULL,
+	"claims" varchar NOT NULL,
+	"grant_type" varchar NOT NULL,
+	"policy_uri" varchar,
+	"logo_uri" varchar,
+	"home_page_uri" varchar,
+	"tos_uri" varchar
+);
+--> statement-breakpoint
+CREATE TABLE "oauth_client" (
+	"id" varchar(64) PRIMARY KEY NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"client_secret" varchar,
+	"scopes" varchar NOT NULL,
+	"jwks_uri" varchar NOT NULL,
+	"application_type" "oauth_application_type" NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "oauth_contacts" (
+	"client_id" varchar(64) NOT NULL,
+	"email" varchar NOT NULL,
+	CONSTRAINT "oauth_contacts_client_id_email_pk" PRIMARY KEY("client_id","email")
+);
+--> statement-breakpoint
+CREATE TABLE "oauth_redirect_uris" (
+	"client_id" varchar(64) NOT NULL,
+	"uri" varchar NOT NULL,
+	CONSTRAINT "oauth_redirect_uris_client_id_uri_pk" PRIMARY KEY("client_id","uri")
+);
+--> statement-breakpoint
+ALTER TABLE "users" DROP CONSTRAINT "users_id_not_null";--> statement-breakpoint
+ALTER TABLE "users" DROP CONSTRAINT "users_username_not_null";--> statement-breakpoint
+ALTER TABLE "users" DROP CONSTRAINT "users_password_hash_not_null";--> statement-breakpoint
+ALTER TABLE "users" DROP CONSTRAINT "users_system_role_not_null";--> statement-breakpoint
+ALTER TABLE "lti_deployments" ADD CONSTRAINT "lti_deployments_client_id_lti_tools_id_fk" FOREIGN KEY ("client_id") REFERENCES "public"."lti_tools"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "lti_tool_supported_message_role" ADD CONSTRAINT "lti_tool_supported_message_role_client_id_lti_tools_id_fk" FOREIGN KEY ("client_id") REFERENCES "public"."lti_tools"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "lti_tool_supported_messages" ADD CONSTRAINT "lti_tool_supported_messages_client_id_lti_tools_id_fk" FOREIGN KEY ("client_id") REFERENCES "public"."lti_tools"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "lti_tools" ADD CONSTRAINT "lti_tools_id_oauth_client_id_fk" FOREIGN KEY ("id") REFERENCES "public"."oauth_client"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "oauth_contacts" ADD CONSTRAINT "oauth_contacts_client_id_oauth_client_id_fk" FOREIGN KEY ("client_id") REFERENCES "public"."oauth_client"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "oauth_redirect_uris" ADD CONSTRAINT "oauth_redirect_uris_client_id_oauth_client_id_fk" FOREIGN KEY ("client_id") REFERENCES "public"."oauth_client"("id") ON DELETE cascade ON UPDATE no action;
--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,8 +1,9 @@
-import { sql } from "drizzle-orm";
+import { relations } from "drizzle-orm";
 import {
-  check,
+  jsonb,
   pgEnum,
   pgTable,
+  primaryKey,
   timestamp,
   uuid,
   varchar,
@@ -21,38 +22,194 @@ export const personGenderEnum = pgEnum("person_gender", [
   PersonGender.NonBinary,
 ]);
 
-export const usersTable = pgTable(
-  "users",
+export const usersTable = pgTable("users", {
+  // user fields
+  id: uuid().primaryKey(),
+  username: varchar({ length: 255 }).notNull().unique(),
+  passwordHash: varchar("password_hash", { length: 120 }).notNull(),
+  profilePictureUrl: varchar("profile_picture_url", { length: 255 }),
+  systemRole: systemRoleEnum("system_role").default(SystemRole.User).notNull(),
+  // person specific fields
+  //
+  // not unique on purpose: in case an user be deactivated at some point,
+  // a new user can still be created with this CPF. Suppose a student
+  // finishes the Ensino Médio but further on becomes a professor or
+  // a parent and need to get a new account on the same school he once was
+  // a student...
+  //
+  // Not any of these can be not null at database because we need to enforce
+  // composition. Required fields MUST be required at software level.
+  cpf: varchar({ length: 11 }),
+  birthDate: timestamp("birth_date", { withTimezone: true }),
+  gender: personGenderEnum(),
+  firstName: varchar("first_name", { length: 255 }),
+  surname: varchar({ length: 255 }),
+  email: varchar({ length: 255 }).unique(),
+});
+
+export const oauthApplicationTypeEnum = pgEnum("oauth_application_type", [
+  "web",
+  "native",
+]);
+
+export const oauthClient = pgTable("oauth_client", {
+  id: varchar({ length: 64 }).primaryKey(),
+  name: varchar({ length: 255 }).notNull(),
+  clientSecret: varchar("client_secret"),
+  scopes: varchar().notNull(),
+  jwksUri: varchar("jwks_uri").notNull(),
+  applicationType: oauthApplicationTypeEnum("application_type").notNull(),
+});
+
+export const ltiTools = pgTable("lti_tools", {
+  id: varchar({ length: 64 })
+    .primaryKey()
+    .references(() => oauthClient.id, { onDelete: "cascade" }),
+  description: varchar(),
+  domain: varchar().notNull(),
+  customParameters: jsonb("custom_parameters"),
+  responseTypes: varchar("response_types").notNull(),
+  initiateUri: varchar("initiate_uri").notNull(),
+  targetLinkUri: varchar("target_link_uri").notNull(),
+  claims: varchar().notNull(),
+  grantTypes: varchar("grant_type").notNull(),
+  policyUri: varchar("policy_uri"),
+  logoUri: varchar("logo_uri"),
+  homePageUri: varchar("home_page_uri"),
+  tosUri: varchar("tos_uri"),
+});
+
+export const oauthRedirectUris = pgTable(
+  "oauth_redirect_uris",
   {
-    // user fields
-    id: uuid().primaryKey().notNull(),
-    username: varchar({ length: 255 }).notNull().unique(),
-    passwordHash: varchar("password_hash", { length: 120 }).notNull(),
-    profilePictureUrl: varchar("profile_picture_url", { length: 255 }),
-    systemRole: systemRoleEnum("system_role")
-      .default(SystemRole.User)
-      .notNull(),
-    // person specific fields
-    //
-    // not unique on purpose: in case an user be deactivated at some point,
-    // a new user can still be created with this CPF. Suppose a student
-    // finishes the Ensino Médio but further on becomes a professor or
-    // a parent and need to get a new account on the same school he once was
-    // a student...
-    //
-    // Not any of these can be not null at database because we need to enforce
-    // composition. Required fields MUST be required at software level.
-    cpf: varchar({ length: 11 }),
-    birthDate: timestamp("birth_date", { withTimezone: true }),
-    gender: personGenderEnum(),
-    firstName: varchar("first_name", { length: 255 }),
-    surname: varchar({ length: 255 }),
-    email: varchar({ length: 255 }).unique(),
+    clientId: varchar("client_id", { length: 64 })
+      .notNull()
+      .references(() => oauthClient.id, { onDelete: "cascade" }),
+    uri: varchar().notNull(),
   },
-  (_table) => [
-    check("users_id_not_null", sql`NOT NULL id`),
-    check("users_username_not_null", sql`NOT NULL username`),
-    check("users_password_hash_not_null", sql`NOT NULL password_hash`),
-    check("users_system_role_not_null", sql`NOT NULL system_role`),
+  (table) => [primaryKey({ columns: [table.clientId, table.uri] })],
+);
+
+export const oauthContacts = pgTable(
+  "oauth_contacts",
+  {
+    clientId: varchar("client_id", { length: 64 })
+      .notNull()
+      .references(() => oauthClient.id, { onDelete: "cascade" }),
+    email: varchar().notNull(),
+  },
+  (table) => [primaryKey({ columns: [table.clientId, table.email] })],
+);
+
+export const ltiToolDeployments = pgTable(
+  "lti_deployments",
+  {
+    clientId: varchar("client_id", { length: 64 })
+      .notNull()
+      .references(() => ltiTools.id, { onDelete: "cascade" }),
+    id: uuid().notNull(),
+  },
+  (table) => [primaryKey({ columns: [table.clientId, table.id] })],
+);
+
+export const ltiToolSupportedMessages = pgTable(
+  "lti_tool_supported_messages",
+  {
+    clientId: varchar("client_id", { length: 64 })
+      .notNull()
+      .references(() => ltiTools.id, { onDelete: "cascade" }),
+    type: varchar().notNull(),
+    targetLinkUri: varchar("target_link_uri"),
+    label: varchar(),
+    iconUri: varchar("icon_uri"),
+    placements: varchar(),
+    customParameters: jsonb("custom_parameters"),
+  },
+  (table) => [primaryKey({ columns: [table.clientId, table.type] })],
+);
+
+export const ltiToolSupportedMessageRoles = pgTable(
+  "lti_tool_supported_message_role",
+  {
+    clientId: varchar("client_id", { length: 64 })
+      .notNull()
+      .references(() => ltiTools.id, { onDelete: "cascade" }),
+    messageType: varchar("msg_type").notNull(),
+    role: varchar().notNull(),
+  },
+  (table) => [
+    primaryKey({ columns: [table.clientId, table.messageType, table.role] }),
   ],
 );
+
+/**
+ * Relations
+ */
+export const ltiToolsRelations = relations(ltiTools, ({ many, one }) => ({
+  oauthClient: one(oauthClient, {
+    fields: [ltiTools.id],
+    references: [oauthClient.id],
+  }),
+  deployments: many(ltiToolDeployments),
+  supportedMessages: many(ltiToolSupportedMessages),
+}));
+
+export const ltiToolDeploymentsRelations = relations(
+  ltiToolDeployments,
+  ({ one }) => ({
+    tool: one(ltiTools, {
+      fields: [ltiToolDeployments.clientId],
+      references: [ltiTools.id],
+    }),
+  }),
+);
+
+export const ltiToolSupportedMessagesRelations = relations(
+  ltiToolSupportedMessages,
+  ({ one, many }) => ({
+    tool: one(ltiTools, {
+      fields: [ltiToolSupportedMessages.clientId],
+      references: [ltiTools.id],
+    }),
+    roles: many(ltiToolSupportedMessageRoles),
+  }),
+);
+
+export const ltiToolSupportedMessageRolesRelations = relations(
+  ltiToolSupportedMessageRoles,
+  ({ one }) => ({
+    message: one(ltiToolSupportedMessages, {
+      fields: [
+        ltiToolSupportedMessageRoles.clientId,
+        ltiToolSupportedMessageRoles.messageType,
+      ],
+      references: [
+        ltiToolSupportedMessages.clientId,
+        ltiToolSupportedMessages.type,
+      ],
+    }),
+  }),
+);
+
+export const oauthClientRelations = relations(oauthClient, ({ one, many }) => ({
+  ltiTool: one(ltiTools),
+  redirectUris: many(oauthRedirectUris),
+  contacts: many(oauthContacts),
+}));
+
+export const oauthRedirectUrisRelations = relations(
+  oauthRedirectUris,
+  ({ one }) => ({
+    client: one(oauthClient, {
+      fields: [oauthRedirectUris.clientId],
+      references: [oauthClient.id],
+    }),
+  }),
+);
+
+export const oauthContactsRelations = relations(oauthContacts, ({ one }) => ({
+  client: one(oauthClient, {
+    fields: [oauthContacts.clientId],
+    references: [oauthClient.id],
+  }),
+}));
