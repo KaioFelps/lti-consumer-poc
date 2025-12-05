4a0343df86f749bdc9a10ad7156c8baf6328afba
Kaio Felps <kaiofelipe2005@gmail.com>
2025-12-05 13:03:14 -0300

    chore: replace jest with vitest

---
+++ vitest.e2e.config.ts
@@ -0,0 +1,13 @@
+import tsConfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    include: ["**/*.e2e-spec.ts"],
+    globals: true,
+    root: "./",
+    // setupFiles: ["./test/test-e2e.ts"],
+    testTimeout: 40000,
+  },
+  plugins: [tsConfigPaths()],
+});
