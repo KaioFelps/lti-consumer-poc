2866b44b3f9c98668bca838fb3d9c3abd62e47fe
Kaio Felps <kaiofelipe2005@gmail.com>
2025-12-23 00:31:18 -0300

    feat(ltilib): add initiate launch & third party login messages

---
+++ ltilib/src/core/messages/initiate-launch.ts
@@ -0,0 +1,63 @@
+import { LtiInitiationMessage } from "$/security/lti-message";
+import {
+  CreateInitiateThirdPartyLoginRequestArgs,
+  LtiInitiateThirdPartyLoginRequest,
+} from "$/security/messages/initiate-third-party-login";
+import type { LTIResourceLinkLaunchRequest } from "./resource-link-launch";
+
+type CreateInitiateLaunchRequestArgs =
+  CreateInitiateThirdPartyLoginRequestArgs & {
+    /**
+     * The same deployment ID that will be passed to the {@link LTIResourceLinkLaunchRequest}
+     * further on.
+     *
+     * @see {@link https://www.imsglobal.org/spec/lti/v1p3/#lti_deployment_id-login-parameter}
+     */
+    deploymentId?: string;
+    /**
+     * Information a platform may want or need to receive back in the subsequent
+     * message.
+     *
+     * @see {@link https://www.imsglobal.org/spec/lti/v1p3/#lti_message_hint-login-parameter}
+     */
+    ltiMessageHint?: string;
+  };
+
+export class InitiateLaunchRequest implements LtiInitiationMessage {
+  protected constructor(
+    private initiateLoginRequest: LtiInitiateThirdPartyLoginRequest,
+    private clientId: string,
+    private deploymentId?: string,
+    private ltiMessageHint?: string,
+  ) {}
+
+  public static create({
+    deploymentId,
+    ltiMessageHint,
+    ...args
+  }: CreateInitiateLaunchRequestArgs) {
+    const initiateLoginRequest = LtiInitiateThirdPartyLoginRequest.create(args);
+    return new InitiateLaunchRequest(
+      initiateLoginRequest,
+      args.tool.id,
+      deploymentId,
+      ltiMessageHint,
+    );
+  }
+
+  public intoUrl() {
+    const url = this.initiateLoginRequest.intoUrl();
+
+    if (this.ltiMessageHint) {
+      url.searchParams.set("lti_message_hint", this.ltiMessageHint);
+    }
+
+    if (this.deploymentId) {
+      url.searchParams.set("lti_deployment_id", this.deploymentId);
+    }
+
+    url.searchParams.set("client_id", this.clientId);
+
+    return url;
+  }
+}
---
+++ ltilib/src/security/messages/initiate-third-party-login.ts
@@ -0,0 +1,41 @@
+import { ToolRecord } from "$/registration/tool-record";
+import { Platform } from "../../core/platform";
+import { LtiInitiationMessage } from "../lti-message";
+
+export type CreateInitiateThirdPartyLoginRequestArgs = {
+  tool: Readonly<ToolRecord>;
+  platform: Readonly<Platform>;
+  loginHint: string;
+  targetLink: URL;
+};
+
+export class LtiInitiateThirdPartyLoginRequest implements LtiInitiationMessage {
+  protected constructor(
+    private tool: Readonly<ToolRecord>,
+    private platform: Readonly<Platform>,
+    private loginHint: string,
+    private targetLink: URL,
+  ) {}
+
+  public static create({
+    loginHint,
+    platform,
+    targetLink,
+    tool,
+  }: CreateInitiateThirdPartyLoginRequestArgs) {
+    return new LtiInitiateThirdPartyLoginRequest(
+      tool,
+      platform,
+      loginHint,
+      targetLink,
+    );
+  }
+
+  public intoUrl(): URL {
+    const url = new URL(this.tool.uris.initiate);
+    url.searchParams.set("iss", this.platform.issuer);
+    url.searchParams.set("login_hint", this.loginHint);
+    url.searchParams.set("target_link_uri", this.targetLink.toString());
+    return url;
+  }
+}
