517f3e87be2ccedcb7414bc5011d2432c53ed601
Kaio Felps <kaiofelipe2005@gmail.com>
2026-01-14 16:20:10 -0300

    feat(MVC routes): add `Rest` decorator to override mvc controllers

--- src/lib/mvc-routes/index.ts
+++ src/lib/mvc-routes/index.ts
@@ -5,6 +5,7 @@ const HANDLER_KEY = "__handler_metadata_is_mvc";
 const METADATA_KEY = "isMVCRoute";
 
 export const Mvc = () => SetMetadata(METADATA_KEY, true);
+export const Rest = () => SetMetadata(METADATA_KEY, false);
 
 export default {
   Guard: MvcGuard,
