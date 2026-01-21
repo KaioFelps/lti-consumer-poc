188e83ed1a5ab3b9894f1a1cf060b5df2bcd4e94
Kaio Felps <kaiofelipe2005@gmail.com>
2026-01-21 19:07:04 -0300

    refactor(tool-details): move translations to ejs view

--- src/lti/tools/tools.controller.ts
+++ src/lti/tools/tools.controller.ts
@@ -134,87 +134,19 @@ export class LtiToolsController {
   public async showToolDetails(@Param("id") toolId: string) {
     const toolDetails = pipe(
       await this.getToolDetailsService.exec({ toolId }),
-      either.foldW(
-        (error) => {
-          throw ExceptionsFactory.fromError(error);
-        },
-        (tool) => tool,
-      ),
+      either.getOrElseW((error) => {
+        throw ExceptionsFactory.fromError(error);
+      }),
     );
 
     return {
       title: await this.t.translate("lti:tools-details:title", {
         toolName: toolDetails.getTool().record.name,
       }),
-      deployPopupTitle: await this.t.translate("lti:deploy-tool:popup-title"),
       tool: LtiToolPresenter.present(toolDetails.getTool()),
       deployments: toolDetails
         .getDeployments()
         .map(LtiToolDeploymentPresenter.present),
-      buttons: {
-        detailsTab: await this.t.translate(
-          "lti:list-tools:buttons:tool-details",
-        ),
-        deploymentsTab: await this.t.translate(
-          "lti:list-tools:buttons:list-deployments",
-        ),
-        deploy: await this.t.translate("lti:tools-details:buttons:new-deploy"),
-        deleteDeployment: await this.t.translate(
-          "lti:tools-details:buttons:delete-deployment",
-        ),
-        confirm: await this.t.translate("buttons:confirm"),
-        cancel: await this.t.translate("buttons:cancel"),
-      },
-      content: {
-        invalidTabSelected: await this.t.translate(
-          "lti:tools-details:invalid-tab-selected",
-        ),
-        noDeploymentsMessage: await this.t.translate(
-          "lti:tools-details:no-tool-deployments",
-        ),
-        deleteToolWarning: await this.t.translate(
-          "lti:delete-tool-deployment:warning-p1",
-        ),
-        unexpectedErrorMessage: await this.t.translate(
-          "core:errors:internal-error-message",
-        ),
-      },
-      toolTableHeadings: {
-        id: await this.t.translate("lti:tools-details:thead:id"),
-        name: await this.t.translate("lti:tools-details:thead:name"),
-        description: await this.t.translate(
-          "lti:tools-details:thead:description",
-        ),
-        grantTypes: await this.t.translate(
-          "lti:tools-details:thead:grant-types",
-        ),
-        initiateUri: await this.t.translate(
-          "lti:tools-details:thead:initiate-uri",
-        ),
-        homePageUri: await this.t.translate(
-          "lti:tools-details:thead:home-page-uri",
-        ),
-        logoUri: await this.t.translate("lti:tools-details:thead:logo-uri"),
-        termsOfServiceUri: await this.t.translate(
-          "lti:tools-details:thead:tos-uri",
-        ),
-        policyUri: await this.t.translate("lti:tools-details:thead:policy-uri"),
-        contacts: await this.t.translate("lti:tools-details:thead:contacts"),
-        registeredMessages: await this.t.translate(
-          "lti:tools-details:thead:registered-msgs",
-        ),
-        requiredClaims: await this.t.translate(
-          "lti:tools-details:thead:required-claims",
-        ),
-      },
-
-      deploymentTableHeadings: {
-        id: await this.t.translate("lti:tools-details:thead:deployment-id"),
-        label: await this.t.translate(
-          "lti:tools-details:thead:deployment-label",
-        ),
-        actions: await this.t.translate("table-headings:actions"),
-      },
     };
   }
 }
