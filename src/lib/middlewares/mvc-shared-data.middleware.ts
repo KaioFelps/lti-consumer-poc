ddf2bba6c86f0afb554fa81e3a8fb1c7aa570ba4
Kaio Felps <kaiofelipe2005@gmail.com>
2026-01-21 18:47:22 -0300

    chore: inject `TranslatorService` to ejs locals

--- src/lib/middlewares/mvc-shared-data.middleware.ts
+++ src/lib/middlewares/mvc-shared-data.middleware.ts
@@ -1,11 +1,15 @@
-import { NestMiddleware } from "@nestjs/common";
+import { Injectable, NestMiddleware } from "@nestjs/common";
 import { UserPresenter } from "@/external/presenters/entities/user.presenter";
 import { User } from "@/identity/user/user.entity";
 import { DEFAULT_EJS_LAYOUT } from "@/main";
+import { TranslatorService } from "@/message-string/translator.service";
 import { Routes } from "@/routes";
 import { HttpRequest, HttpResponse, RequestSession } from "..";
 
+@Injectable()
 export class MvcSharedDataMiddleware implements NestMiddleware {
+  private constructor(private t: TranslatorService) {}
+
   use(req: HttpRequest, res: HttpResponse, next: (error?: unknown) => void) {
     const sessions = req["session"] as RequestSession;
 
@@ -16,6 +20,7 @@ export class MvcSharedDataMiddleware implements NestMiddleware {
 
     res.locals.layout = DEFAULT_EJS_LAYOUT;
     res.locals.routes = Routes;
+    res.locals.t = this.t;
 
     return next();
   }
