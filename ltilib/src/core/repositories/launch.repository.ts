bbd5579e1fc8355a29f0854093ac478304505f9a
Kaio Felps <kaiofelipe2005@gmail.com>
2026-01-29 01:16:50 -0300

    feat(ltilib): add `LtiLaunchRepository`

---
+++ ltilib/src/core/repositories/launch.repository.ts
@@ -0,0 +1,14 @@
+import { Either } from "fp-ts/lib/Either";
+import { LtiRepositoryError } from "../errors/repository.error";
+import { LtiLaunchData } from "../launch-data";
+
+export abstract class LtiLaunchRepository {
+  public abstract save(
+    launch: LtiLaunchData,
+    timeToLiveSeconds: number,
+  ): Promise<Either<LtiRepositoryError, void>>;
+
+  public abstract findById(
+    launchId: string,
+  ): Promise<Either<LtiRepositoryError, LtiLaunchData>>;
+}
