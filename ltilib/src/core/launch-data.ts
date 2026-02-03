84ec719da9ade94038979a3c7afea9f13da9ae3d
Kaio Felps <kaiofelipe2005@gmail.com>
2026-02-03 17:54:22 -0300

    feat(ltilib/`LtiLaunchData`): store launch presentation

--- ltilib/src/core/launch-data.ts
+++ ltilib/src/core/launch-data.ts
@@ -1,6 +1,7 @@
 import { ClassProperties } from "common/src/types/class-properties";
 import { Optional } from "common/src/types/optional";
 import { generateUUID, UUID } from "common/src/types/uuid";
+import { MessageRequests } from "./messages";
 
 type Args = Optional<ClassProperties<LtiLaunchData>, "id">;
 
@@ -9,9 +10,15 @@ export class LtiLaunchData {
     public readonly id: UUID,
     public readonly resourceLinkId: UUID,
     public readonly userId: string,
+    public readonly presentation?: MessageRequests.Presentation,
   ) {}
 
-  public static create({ resourceLinkId, userId, id = generateUUID() }: Args) {
-    return new LtiLaunchData(id, resourceLinkId, userId);
+  public static create({
+    resourceLinkId,
+    userId,
+    presentation,
+    id = generateUUID(),
+  }: Args) {
+    return new LtiLaunchData(id, resourceLinkId, userId, presentation);
   }
 }
