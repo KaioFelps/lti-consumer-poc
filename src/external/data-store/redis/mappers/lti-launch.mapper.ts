d0a5088576278b0eff5ec15256aed1272f78c7f1
Kaio Felps <kaiofelipe2005@gmail.com>
2026-02-03 18:25:41 -0300

    fix(redis `LtiLaunchData` mapper): map presentation

--- src/external/data-store/redis/mappers/lti-launch.mapper.ts
+++ src/external/data-store/redis/mappers/lti-launch.mapper.ts
@@ -1,25 +1,59 @@
 import { option as opt } from "fp-ts";
 import { LtiLaunchData } from "$/core/launch-data";
+import { MessageRequests } from "$/core/messages";
 
 type Payload = {
   launchId: string;
   resourceLinkId: string;
   userId: string;
+  presentation?: {
+    documentTarget?: string;
+    width?: number;
+    height?: number;
+    returnUrl?: string;
+    locale?: string;
+  };
 };
 
 function intoPayload(launch: LtiLaunchData): Payload {
+  const { documentTarget, height, locale, width, returnUrl } =
+    launch.presentation ?? {};
+
   return {
     launchId: launch.id.toString(),
     resourceLinkId: launch.resourceLinkId.toString(),
     userId: launch.userId,
+    presentation: {
+      documentTarget: documentTarget?.toString(),
+      height,
+      locale,
+      width,
+      returnUrl: returnUrl?.toString(),
+    },
   };
 }
 
 function fromPayload(payload: Payload): LtiLaunchData {
+  let presentation: MessageRequests.Presentation | undefined;
+
+  if (payload.presentation) {
+    presentation = MessageRequests.Presentation.create({
+      documentTarget: payload.presentation
+        .documentTarget as MessageRequests.DocumentTarget,
+      width: payload.presentation.width,
+      height: payload.presentation.height,
+      locale: payload.presentation.locale,
+      returnUrl: payload.presentation.returnUrl
+        ? new URL(payload.presentation.returnUrl)
+        : undefined,
+    });
+  }
+
   return LtiLaunchData.create({
     resourceLinkId: payload.resourceLinkId,
     userId: payload.userId,
     id: payload.launchId,
+    presentation,
   });
 }
 
