5c9f3452f6cdd85bc3224b42da85b34dfa2873dc
Kaio Felps <kaiofelipe2005@gmail.com>
2026-02-16 21:45:49 -0300

    refactor(ltilib): split Platform namespace objects across different files

---
+++ ltilib/src/core/platform/index.ts
@@ -0,0 +1,68 @@
+import { JWK } from "jose";
+import * as platformNamespace from "./namespace";
+
+interface IPlatform {
+  /**
+   * The platform's private key used to sign JWSs.
+   */
+  jsonWebKey: JWK;
+  /**
+   * A resolver to the launch initiation endpoint in the platform.
+   */
+  initiateLaunchEndpoint: (resourceLinkId: string) => string;
+  /**
+   * Represents an instance of the platform.
+   *
+   * @see {@link Platform.Instance}
+   * @see {@link https://www.cloudflare.com/learning/cloud/what-is-multitenancy/ Multitenancy}
+   */
+  instance?: Platform.Instance;
+  /**
+   * The LTI specific OpenID configuration.
+   *
+   * @see {@link Platform.OpenIdConfiguration}
+   */
+  openIdConfiguration: Platform.OpenIdConfiguration;
+}
+
+/**
+ * Represents a LTI Platform.
+ */
+export class Platform implements IPlatform {
+  /**
+   * This platform's public issuer.
+   *
+   * @see {@link Platform.OpenIdConfiguration.issuer}
+   */
+  public readonly issuer: string;
+  /**
+   * The resolved OpenID configuration.
+   * Same as `platform.openIdConfiguration.intoConfiguration()`.
+   */
+  public readonly resolvedOpenIdConfiguration: object;
+
+  public readonly jsonWebKey: JWK;
+  public readonly initiateLaunchEndpoint: IPlatform["initiateLaunchEndpoint"];
+  public readonly instance?: Platform.Instance | undefined;
+  public readonly openIdConfiguration: Platform.OpenIdConfiguration;
+
+  protected constructor(args: IPlatform) {
+    Object.assign(this, args);
+    this.issuer = args.openIdConfiguration.issuer;
+    this.resolvedOpenIdConfiguration = args.openIdConfiguration.intoConfiguration();
+  }
+
+  public static create(args: IPlatform) {
+    return new Platform(args);
+  }
+}
+
+export namespace Platform {
+  export const { Instance, OpenIdConfiguration, LtiAssignmentAndGradeServicesConfig } =
+    platformNamespace;
+
+  export type Instance = platformNamespace.Instance;
+  export type OpenIdConfiguration = platformNamespace.OpenIdConfiguration;
+  export type LtiAssignmentAndGradeServicesConfig =
+    platformNamespace.LtiAssignmentAndGradeServicesConfig;
+}
---
+++ ltilib/src/core/platform/instance.ts
@@ -0,0 +1,75 @@
+import { ClassProperties } from "common/src/types/class-properties";
+import { IntoLtiClaim } from "$/claims/serialization";
+
+type InstanceConstructorArgs = ClassProperties<Instance>;
+
+/*
+ * When in multi-tenancy case, the platform instance initiating the launch can identify itself
+ * through the [Platform instance claim]. If present, this `Platform.Instance` will compose
+ * that claim.
+ *
+ * [Platform instance claim]: https://www.imsglobal.org/spec/lti/v1p3/#platform-instance-claim
+ */
+export class Instance implements IntoLtiClaim {
+  /**
+   * A globally unique ID that identifies the platform instance within the issuer
+   * (the platform itself). This field must contain only ASCII characters and must be no longer
+   * than 255 characters.
+   *
+   * E.g.: three institutions use your platform, however each of them uses a virtual instance
+   * of it, and each virtual instance has an unique ID within the platform.
+   */
+  public readonly guid: string;
+  /**
+   * E-mail address for administrative issues.
+   */
+  public readonly contactEmail?: string;
+  /**
+   * Description of the plataform instance.
+   */
+  public readonly description?: string;
+  /**
+   * Name of this instance of the platform.
+   */
+  public readonly name?: string;
+  /**
+   * The URL to the platform instance's home page.
+   *
+   * @example "https://<institution>.platform.com"
+   */
+  public readonly url?: URL;
+  /**
+   * Vendor's family code for the platform product line
+   * (that the instance that triggered the launch belongs to).
+   *
+   * @example "moodle"
+   */
+  public readonly productFamilyCode?: string;
+  /**
+   * Vendor's version of the underlying platform which instance
+   * has triggered the launch.
+   *
+   * @example "5.1"
+   */
+  public readonly version?: string;
+
+  protected constructor(args: InstanceConstructorArgs) {
+    Object.assign(this, args);
+  }
+
+  public static create(args: InstanceConstructorArgs) {
+    return new Instance(args);
+  }
+
+  intoLtiClaim(): object {
+    return {
+      guid: this.guid,
+      contact_email: this.contactEmail,
+      description: this.description,
+      name: this.name,
+      url: this.url?.toString(),
+      product_family_code: this.productFamilyCode,
+      version: this.version,
+    };
+  }
+}
