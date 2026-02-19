4f04d03b30d2723bc16c3796edd8b06404b33861
Kaio Felps <kaiofelipe2005@gmail.com>
2026-02-18 22:14:47 -0300

    chore: add `AllOptional` type

--- common/src/types/optional.ts
+++ common/src/types/optional.ts
@@ -14,3 +14,10 @@
  **/
 
 export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
+
+/**
+ * Make every property from `T` optional.
+ */
+export type AllOptional<T> = {
+  [K in keyof T]?: T[K] | undefined;
+};
