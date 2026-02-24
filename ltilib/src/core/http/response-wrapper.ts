f49be2bc60a066c8cad68c49885b32add7789e12
Kaio Felps <kaiofelipe2005@gmail.com>
2026-02-24 15:43:16 -0300

    feat(ltilib): add `HttpResponseWrapper` to enforce http status and headers that are required by LTI specifications

---
+++ ltilib/src/core/http/response-wrapper.ts
@@ -0,0 +1,12 @@
+export class HttpResponseWrapper<RawContent = undefined, PresentedContent = object> {
+  public constructor(
+    public readonly content: PresentedContent,
+    public readonly httpStatus: number,
+    public readonly rawContent: RawContent,
+    public headers: Record<string, string> = {},
+  ) {}
+
+  public appendHeader(key: string, value: string) {
+    this.headers[key] = value;
+  }
+}
