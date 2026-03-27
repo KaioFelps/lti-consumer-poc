4bf2b10bfc81a9e457094b91d8b79c0bdd3b183d
Kaio Felps <kaiofelipe2005@gmail.com>
2026-03-27 20:29:19 -0300

    refactor(ltilib/AGS/errors): `CannotAttachResourceLinkError` now inherits `InvalidLineItemArgumentError` & allow `resourceId` as a valid `field` of the latter error

--- ltilib/src/assignment-and-grade/errors/cannot-attach-resource-link.error.ts
+++ ltilib/src/assignment-and-grade/errors/cannot-attach-resource-link.error.ts
@@ -1,6 +1,4 @@
-import { LtilibError } from "$/core/errors/bases/ltilib.error";
-
-type Reason = "doesnt_belong_to_tool" | "doesnt_belong_to_lineitem_context";
+import { InvalidLineItemArgumentError } from "./invalid-line-item-argument.error";
 
 /**
  * Line item cannot be attached to resource link because either don't they belong
@@ -9,9 +7,9 @@ type Reason = "doesnt_belong_to_tool" | "doesnt_belong_to_lineitem_context";
  *
  * @see {@link https://www.imsglobal.org/spec/lti-ags/v2p0#resourcelinkid-and-binding-a-line-item-to-a-resource-link section 3.2.9 of the LTI AGS specification}
  */
-export class CannotAttachResourceLinkError extends LtilibError {
-  public readonly field = "resourceLinkId";
-  public constructor(public readonly reason: Reason) {
-    super(404, reason);
+export class CannotAttachResourceLinkError extends InvalidLineItemArgumentError {
+  public constructor(reason: InvalidLineItemArgumentError.Codes["resourceLinkId"][number]) {
+    super("resourceLinkId", reason);
+    this._httpStatusCode = 404;
   }
 }
--- ltilib/src/assignment-and-grade/errors/invalid-line-item-argument.error.ts
+++ ltilib/src/assignment-and-grade/errors/invalid-line-item-argument.error.ts
@@ -1,15 +1,24 @@
 import { InvalidArgumentError } from "$/core/errors/bases/invalid-argument.error";
 
-type Fields = "label" | "scoreMaximum" | "customParameters";
-
-const codes = {
-  label: ["required"],
-  scoreMaximum: ["must_be_greater_than_zero", "required"],
-  customParameters: ["key_must_be_fully_qualified_url"],
-} as const satisfies Record<Fields, string[]>;
-
-type Codes = typeof codes;
-
 export class InvalidLineItemArgumentError<
-  Field extends Fields = Fields,
-> extends InvalidArgumentError<Field, Codes[Field][number]> {}
+  Field extends InvalidLineItemArgumentError.Fields = InvalidLineItemArgumentError.Fields,
+> extends InvalidArgumentError<Field, InvalidLineItemArgumentError.Codes[Field][number]> {}
+
+export namespace InvalidLineItemArgumentError {
+  export type Fields =
+    | "label"
+    | "scoreMaximum"
+    | "customParameters"
+    | "resourceLinkId"
+    | "resourceId";
+
+  export type Codes = typeof codes;
+
+  const codes = {
+    label: ["required"],
+    scoreMaximum: ["must_be_greater_than_zero", "required"],
+    customParameters: ["key_must_be_fully_qualified_url"],
+    resourceLinkId: ["must_belong_to_tool", "must_belong_to_lineitem_context"],
+    resourceId: ["must_belong_to_tool", "must_belong_to_lineitem_context"],
+  } as const satisfies Record<Fields, string[]>;
+}
