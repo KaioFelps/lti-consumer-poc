2498ae9b689b16e1c842943327a2c5bd21162638
Kaio Felps <kaiofelipe2005@gmail.com>
2026-03-27 20:22:13 -0300

    change(ltilib/`LtilibError`): allow super classes to modify `httpStatusCode`

--- ltilib/src/core/errors/bases/ltilib.error.ts
+++ ltilib/src/core/errors/bases/ltilib.error.ts
@@ -5,10 +5,14 @@ export interface ILtilibError {
 
 export abstract class LtilibError extends Error implements ILtilibError {
   public constructor(
-    public readonly httpStatusCode: number,
+    protected _httpStatusCode: number,
     message?: string,
     errorOptions?: ErrorOptions,
   ) {
     super(message, errorOptions);
+  }
+
+  public get httpStatusCode() {
+    return this._httpStatusCode;
   }
 }
