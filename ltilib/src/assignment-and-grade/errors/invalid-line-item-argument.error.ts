18591bd12ef04e4694d384ba63c2faa08b9c2a06
Kaio Felps <kaiofelipe2005@gmail.com>
2026-03-27 22:56:00 -0300

    remove(ltilib/`InvalidLineItemArgumentError`): disallow `resourceId` field errors

--- ltilib/src/assignment-and-grade/errors/invalid-line-item-argument.error.ts
+++ ltilib/src/assignment-and-grade/errors/invalid-line-item-argument.error.ts
@@ -5,12 +5,13 @@ export class InvalidLineItemArgumentError<
 > extends InvalidArgumentError<Field, InvalidLineItemArgumentError.Codes[Field][number]> {}
 
 export namespace InvalidLineItemArgumentError {
-  export type Fields =
-    | "label"
-    | "scoreMaximum"
-    | "customParameters"
-    | "resourceLinkId"
-    | "resourceId";
+  // do not need to handle resource id:
+  // - LTI does not state that it must or should belong to the same context to the line item,
+  //   since it's something internal to the tool;
+  // - it **will always** belong to the tool making the request because the service and repository
+  //   searches for the tuple (resourceId, toolId), hence if it does not belong to the tool it
+  //   won't even be found...
+  export type Fields = "label" | "scoreMaximum" | "customParameters" | "resourceLinkId";
 
   export type Codes = typeof codes;
 
@@ -19,6 +20,5 @@ export namespace InvalidLineItemArgumentError {
     scoreMaximum: ["must_be_greater_than_zero", "required"],
     customParameters: ["key_must_be_fully_qualified_url"],
     resourceLinkId: ["must_belong_to_tool", "must_belong_to_lineitem_context"],
-    resourceId: ["must_belong_to_tool", "must_belong_to_lineitem_context"],
   } as const satisfies Record<Fields, string[]>;
 }
