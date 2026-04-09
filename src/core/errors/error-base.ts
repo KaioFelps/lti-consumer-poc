146a6f74090e09f7795c411a8b61682ed6a1d751
Kaio Felps <kaiofelipe2005@gmail.com>
2026-04-09 01:58:43 -0300

    change(`ErrorBase`): add interface `IErrorBase` and native getters for properties, keeping an internal protected state to allow super classes to modify its properties

--- src/core/errors/error-base.ts
+++ src/core/errors/error-base.ts
@@ -3,6 +3,24 @@ import { MessageStringFormatterArg } from "@/message-string/internal";
 
 export type ErrorClassProperties<Class> = Omit<ClassProperties<Class>, "errorType">;
 
+export interface IErrorBase {
+  /**
+   * A domain-namespaced error code that may be used by a client to find and display a
+   * message string that explains this error.
+   */
+  errorMessageIdentifier: string;
+  /**
+   * Optional parameters that the message-strings module may use when formatting
+   * the message identified by `errorMessageIdentifier`.
+   */
+  messageParams?: MessageStringFormatterArg;
+  /**
+   * A code that indicates the type of this error. May be used by
+   * a client to determine the appropriate way to handle this error.
+   */
+  errorType: string;
+}
+
 /**
  * `ErrorBase` is the base class for every core-level error. It models
  * the minimum data every error should hold in order to be meaningful.
@@ -13,25 +31,26 @@ export type ErrorClassProperties<Class> = Omit<ClassProperties<Class>, "errorTyp
  * TypeScript's `Required` type) or adding extra properties and adding
  * them to message params inside its constructor.
  */
-export abstract class ErrorBase {
-  /**
-   * A domain-namespaced error code that may be used by a client to find and display a
-   * message string that explains this error.
-   */
-  public readonly errorMessageIdentifier: string;
-  /**
-   * Optional parameters that the message-strings module may use when formatting
-   * the message identified by `errorMessageIdentifier`.
-   */
-  public readonly messageParams?: MessageStringFormatterArg;
-  /**
-   * A code that indicates the type of this error. May be used by
-   * a client to determine the appropriate way to handle this error.
-   */
-  public readonly errorType: string;
+export abstract class ErrorBase implements IErrorBase {
+  protected _errorMessageIdentifier: string;
+  protected _messageParams?: MessageStringFormatterArg;
+  protected _errorType: string;
 
-  protected constructor(properties: ErrorClassProperties<ErrorBase>, errorType: string) {
-    Object.assign(this, properties);
-    this.errorType = errorType;
+  protected constructor(props: Omit<IErrorBase, "errorType">, errorType: string) {
+    this._errorMessageIdentifier = props.errorMessageIdentifier;
+    this._errorType = errorType;
+    this._messageParams = props.messageParams;
+  }
+
+  public get errorMessageIdentifier() {
+    return this._errorMessageIdentifier;
+  }
+
+  public get messageParams() {
+    return this._messageParams;
+  }
+
+  public get errorType() {
+    return this._errorType;
   }
 }
