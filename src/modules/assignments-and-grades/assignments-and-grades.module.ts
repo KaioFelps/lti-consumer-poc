26701f1c211abf0eff4db6675800e22eaf2a698c
Kaio Felps <kaiofelipe2005@gmail.com>
2026-05-02 23:08:32 -0300

    fix: handle cyclic module import between courses and assignments modules

--- src/modules/assignments-and-grades/assignments-and-grades.module.ts
+++ src/modules/assignments-and-grades/assignments-and-grades.module.ts
@@ -1,11 +1,13 @@
-import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { forwardRef, MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import middlewares from "@/lib/middlewares";
+import { CoursesModule } from "../courses-and-enrollments/courses.module";
 import { AssignmentsController } from "./assignments.controller";
 import { CreateAssignmentService } from "./services/create-assignment.service";
 import { GradeAnAssignmentService } from "./services/grade-an-assignment.service";
 import { ListAssignmentsFromCourseService } from "./services/list-assignments-from-course.service";
 
 @Module({
+  imports: [forwardRef(() => CoursesModule)],
   providers: [CreateAssignmentService, GradeAnAssignmentService, ListAssignmentsFromCourseService],
   controllers: [AssignmentsController],
   exports: [ListAssignmentsFromCourseService],
--- src/modules/courses-and-enrollments/courses.module.ts
+++ src/modules/courses-and-enrollments/courses.module.ts
@@ -1,11 +1,13 @@
-import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { forwardRef, MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import middlewares from "@/lib/middlewares";
+import { AssignmentsAndGradesModule } from "../assignments-and-grades/assignments-and-grades.module";
 import { CoursesAndEnrollmentsController } from "./courses-and-enrollments.controller";
 import { CreateCourseService } from "./services/create-course.service";
 import { FetchManyCoursesService } from "./services/fetch-many-courses.service";
 import { FindCourseByIdService } from "./services/find-course-by-id.service";
 
 @Module({
+  imports: [forwardRef(() => AssignmentsAndGradesModule)],
   providers: [CreateCourseService, FetchManyCoursesService, FindCourseByIdService],
   controllers: [CoursesAndEnrollmentsController],
   exports: [FindCourseByIdService],
