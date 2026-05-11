1505d3f435ce88a26c804e49cc984167dbb0becf
Kaio Felps <kaiofelipe2005@gmail.com>
2026-05-11 16:17:25 -0300

    fix(`CreateExternalLtiAssignmentService`): require composed context id rather than course id directly Course's ID is still required though because platform's assignments must be tied to a course directly. Further on, this platform could have groups within courses and then we would have to replace 'courseId' with another composed id or something, but this would require restructuring the whole application, not only this service, anyway.

--- src/modules/lti/ags/services/create-external-lti-assignment.service.ts
+++ src/modules/lti/ags/services/create-external-lti-assignment.service.ts
@@ -1,25 +1,19 @@
-/**
- * TODO: this should not use course at all, but a context id + context type instead
- * in order to support different contexts...
- */
 import { Injectable } from "@nestjs/common";
 import type { UUID } from "common/src/types/uuid";
 import { taskEither as te } from "fp-ts";
 import { pipe } from "fp-ts/lib/function";
 import { ResourceNotFoundError } from "@/core/errors/resource-not-found.error";
 import { TransactionManager } from "@/core/transaction-manager";
-import { CoursesRepository } from "@/modules/assignments-and-grades/repositories/courses.repository";
 import { CreateAssignmentService } from "@/modules/assignments-and-grades/services/create-assignment.service";
-import { Course } from "@/modules/courses-and-enrollments/entities/course.entity";
+import { unmountContextId } from "../../advantage/context";
 import { CreateResourceLinkService } from "../../resource-links/services/create-resource-link.service";
 import { LtiToolDeployment } from "../../tools/entities/lti-tool-deployment.entity";
 import { LtiToolsDeploymentsRepository } from "../../tools/lti-tools-deployments.repository";
-import { ContextConcreteType } from "../enums/context-concrete-type";
 import { ToolNotDeployedError } from "../errors/tool-not-deployed.error";
 import { ExternalLtiAssignmentsRepository } from "../repositories/external-lti-assignments.repository";
 
 type Params = {
-  courseId: UUID;
+  contextComposedId: string;
   instructorId: UUID;
   toolId: string;
   assignment: {
@@ -27,6 +21,7 @@ type Params = {
     maxScore: number;
     releasedAt?: Date;
     deadline?: Date;
+    courseId: UUID;
   };
   resourceLink: {
     resourceUrl?: URL;
@@ -38,19 +33,17 @@ type Params = {
 export class CreateExternalLtiAssignmentService {
   public constructor(
     private readonly deploymentsRepository: LtiToolsDeploymentsRepository,
-    private readonly coursesRepository: CoursesRepository,
     private readonly transactionManager: TransactionManager,
     private readonly externalAssignmentsRepo: ExternalLtiAssignmentsRepository,
     private readonly createResourceLinkService: CreateResourceLinkService,
     private readonly createAssignmentService: CreateAssignmentService,
   ) {}
 
-  public execute(params: Params) {
-    const { toolId, courseId } = params;
+  public exec(params: Params) {
+    const { toolId, contextComposedId } = params;
     return pipe(
       te.Do,
-      te.apS("course", () => this.coursesRepository.findById(courseId)),
-      te.bindW("deployment", ({ course }) => this.findToolDeployment(toolId, course)),
+      te.bindW("deployment", () => this.findToolDeployment(toolId, contextComposedId)),
       te.chainW(({ deployment }) =>
         this.persistAssignmentAndResourceLinkAtomically(params, deployment),
       ),
@@ -61,13 +54,13 @@ export class CreateExternalLtiAssignmentService {
     params: Params,
     deployment: LtiToolDeployment,
   ) {
-    const { assignment, instructorId, courseId, resourceLink } = params;
+    const { assignment, instructorId, resourceLink } = params;
     return () =>
       this.transactionManager.runInTransaction(async () => {
         return pipe(
           te.Do,
           te.apS("assignment", () =>
-            this.createAssignmentService.execute({ ...assignment, instructorId, courseId }),
+            this.createAssignmentService.execute({ ...assignment, instructorId }),
           ),
           te.bindW(
             "resourceLink",
@@ -86,14 +79,19 @@ export class CreateExternalLtiAssignmentService {
       });
   }
 
-  private findToolDeployment(toolId: string, course: Course) {
+  private findToolDeployment(toolId: string, contextComposedId: string) {
     return pipe(
-      () =>
-        this.deploymentsRepository.findMostAppropriateDeploymentForTool(
-          toolId,
-          course.getId().toString(),
-          ContextConcreteType.Course,
-        ),
+      unmountContextId(contextComposedId),
+      te.fromEither,
+      te.chainW(
+        ({ concreteEntityId, concreteType }) =>
+          () =>
+            this.deploymentsRepository.findMostAppropriateDeploymentForTool(
+              toolId,
+              concreteEntityId,
+              concreteType,
+            ),
+      ),
       te.mapLeft((error) =>
         error instanceof ResourceNotFoundError ? new ToolNotDeployedError(toolId) : error,
       ),
