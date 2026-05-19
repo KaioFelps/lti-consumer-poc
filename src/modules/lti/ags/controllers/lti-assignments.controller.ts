c6e7dc41761e3639cccb892eb45cc4364bce2a37
Kaio Felps <kaiofelipe2005@gmail.com>
2026-05-19 03:17:28 -0300

    feat: add route for viewing a external LTI assignment in details + launching it

--- src/modules/lti/ags/controllers/lti-assignments.controller.ts
+++ src/modules/lti/ags/controllers/lti-assignments.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Param, Post, Render, Res, Session } from "@nestj
 import type { UUID } from "common/src/types/uuid";
 import { taskEither as te } from "fp-ts";
 import { pipe } from "fp-ts/lib/function";
+import { AssignmentPresenter } from "@/external/presenters/entities/assignment.presenter";
 import { CourseWithInstructorPresenter } from "@/external/presenters/entities/course-with-instructor.presenter";
 import { LtiToolPreviewPresenter } from "@/external/presenters/entities/lti-tool-preview.presenter";
 import { HttpResponse, RequestSession } from "@/lib";
@@ -9,23 +10,30 @@ import { ExceptionsFactory } from "@/lib/exceptions/exceptions.factory";
 import { Mvc } from "@/lib/mvc-routes";
 import { TranslatorService } from "@/message-string/translator.service";
 import { FindCourseByIdService } from "@/modules/courses-and-enrollments/services/find-course-by-id.service";
+import { LtiRepositoryError } from "$/core/errors/repository.error";
+import { Platform } from "$/core/platform";
+import { presentLtiResourceLink } from "$/core/presenters/resource-link.presenter";
 import { mountContextId } from "../../advantage/context";
 import { FindManyToolsPreviewsService } from "../../tools/services/find-many-tools-previews.service";
 import { CreateExternalLtiAssignmentDto } from "../dtos/create-external-lti-assignment.dto";
+import { ShowAssignmentsDetailsDto } from "../dtos/show-assignments-details-params.dto";
 import { ContextConcreteType } from "../enums/context-concrete-type";
 import { CreateExternalLtiAssignmentService } from "../services/create-external-lti-assignment.service";
+import { FindExternalLtiAssignmentByIdService } from "../services/find-external-lti-assignment-by-id.service";
 
 @Mvc()
-@Controller("lti/ags/assignments")
+@Controller("lti/ags")
 export class LtiAssignmentsController {
   public constructor(
     private readonly t: TranslatorService,
+    private readonly platform: Platform,
     private readonly findToolsService: FindManyToolsPreviewsService,
     private readonly findCourseByIdService: FindCourseByIdService,
     private readonly createExternalLtiAssignmentService: CreateExternalLtiAssignmentService,
+    private readonly findExternalLtiAssignmentService: FindExternalLtiAssignmentByIdService,
   ) {}
 
-  @Get("courses/:courseId/new")
+  @Get("courses/:courseId/assignments/new")
   @Render("assignments/new-lti")
   public async showCreateNewExternalAssignmentForm(@Param("courseId") courseId: UUID) {
     const title = await this.t.translate("lti:ags:create-assignment:title");
@@ -50,7 +58,7 @@ export class LtiAssignmentsController {
     )();
   }
 
-  @Post("courses/:courseId/new")
+  @Post("courses/:courseId/assignments/new")
   public async storeNewExternalAssignment(
     @Body() body: CreateExternalLtiAssignmentDto,
     @Param("courseId") courseId: UUID,
@@ -89,5 +97,36 @@ export class LtiAssignmentsController {
     };
 
     response.redirectBack();
+  }
+
+  @Render("lti/assignments/details")
+  @Get("/assignments/:assignmentId")
+  public showAssignmentDetails(@Param() { assignmentId }: ShowAssignmentsDetailsDto) {
+    return pipe(
+      te.Do,
+      te.apS("externalAssignmentRecord", () =>
+        this.findExternalLtiAssignmentService.exec({ assignmentId }),
+      ),
+      te.bindW("course", ({ externalAssignmentRecord }) => {
+        const courseId = externalAssignmentRecord.assignment.getCourseId();
+        return courseId ? () => this.findCourseByIdService.exec({ courseId }) : te.right(undefined);
+      }),
+      te.chainTaskK(({ course, externalAssignmentRecord }) => async () => ({
+        title: await this.t.translate("lti:ags:show-assignment:title", {
+          courseTitle: course?.getCourse().getTitle(),
+          assignmentTitle: externalAssignmentRecord.assignment.getTitle(),
+        }),
+        course: course ? CourseWithInstructorPresenter.present(course) : undefined,
+        assignment: AssignmentPresenter.present(externalAssignmentRecord.assignment),
+        ltiResourceLink: presentLtiResourceLink(
+          externalAssignmentRecord.resourceLink,
+          this.platform,
+        ),
+      })),
+      te.getOrElse((error) => {
+        const actualError = error instanceof LtiRepositoryError ? error.cause : error;
+        throw ExceptionsFactory.fromError(actualError);
+      }),
+    )();
   }
 }
