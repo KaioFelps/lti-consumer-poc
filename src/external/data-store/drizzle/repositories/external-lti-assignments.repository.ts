828a2143d8a6b245c00a916f25280145f128c433
Kaio Felps <kaiofelipe2005@gmail.com>
2026-05-19 03:13:02 -0300

    feat: implements `DrizzleExternalLtiAssignmentsRepository::findCompleteExternalLtiAssignmentById`

--- src/external/data-store/drizzle/repositories/external-lti-assignments.repository.ts
+++ src/external/data-store/drizzle/repositories/external-lti-assignments.repository.ts
@@ -1,14 +1,18 @@
 import { Injectable } from "@nestjs/common";
+import { UUID } from "common/src/types/uuid";
 import { assignmentsT, ltiAssignmentsT } from "drizzle/schema";
 import { eq } from "drizzle-orm";
-import { taskEither as te } from "fp-ts";
+import { either as e, taskEither as te } from "fp-ts";
 import { Either } from "fp-ts/lib/Either";
 import { pipe } from "fp-ts/lib/function";
 import { IrrecoverableError } from "@/core/errors/irrecoverable-error";
 import { Assignment } from "@/modules/assignments-and-grades/entities/assignment.entity";
+import { ExternalAssignmentNotFoundError } from "@/modules/lti/ags/errors/external-assignment-not-found.error";
 import { ExternalLtiAssignmentsRepository } from "@/modules/lti/ags/repositories/external-lti-assignments.repository";
 import { LtiResourceLink } from "$/core/resource-link";
 import { DrizzleClient } from "../client";
+import assignmentsMapper from "../mappers/assignments.mapper";
+import ltiResourceLinksMapper from "../mappers/lti-resource-links.mapper";
 import { DrizzleTransactionManager } from "../transaction-manager";
 
 @Injectable()
@@ -53,6 +57,36 @@ export class DrizzleExternalLtiAssignmentsRepository extends ExternalLtiAssignme
           ),
       ),
       te.map(() => {}),
+    )();
+  }
+
+  public findCompleteExternalLtiAssignmentById(assignmentId: UUID) {
+    const client = this.txManager.getTx() ?? this.drizzle.getClient();
+
+    return pipe(
+      te.tryCatch(
+        () =>
+          client.query.ltiAssignmentsT.findFirst({
+            columns: { assignmentId: false, resourceLinkId: false },
+            with: {
+              assignmemnt: assignmentsMapper.requiredQueryConfig,
+              resourceLink: ltiResourceLinksMapper.requiredQueryConfig,
+            },
+          }),
+        (error) =>
+          new IrrecoverableError(
+            `Error occurred in ${DrizzleExternalLtiAssignmentsRepository.name} when ` +
+              `finding complete external LTI assignment by id "${assignmentId}".`,
+            error as Error,
+          ),
+      ),
+      te.chainEitherKW((row) => {
+        if (!row) return e.left(new ExternalAssignmentNotFoundError(assignmentId));
+        return e.right({
+          assignment: assignmentsMapper.fromRow(row.assignmemnt),
+          resourceLink: ltiResourceLinksMapper.fromRow(row.resourceLink),
+        });
+      }),
     )();
   }
 }
