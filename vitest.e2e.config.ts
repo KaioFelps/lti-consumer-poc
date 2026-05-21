c466f6605af16d1b17c9fc666759018e0e02a78d
Kaio Felps <kaiofelipe2005@gmail.com>
2026-05-20 22:40:44 -0300

    chore: setup vitest with swc

--- vitest.e2e.config.ts
+++ vitest.e2e.config.ts
@@ -1,3 +1,6 @@
+import { configDotenv } from "dotenv";
+import { expand } from "dotenv-expand";
+import swc from "unplugin-swc";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
@@ -9,5 +12,14 @@ export default defineConfig({
     // setupFiles: ["./test/test-e2e.ts"],
     testTimeout: 40000,
   },
-  plugins: [tsConfigPaths()],
+  plugins: [
+    tsConfigPaths(),
+    swc.vite({
+      module: { type: "es6" },
+      jsc: {
+        parser: { syntax: "typescript", decorators: true },
+        transform: { decoratorMetadata: true, legacyDecorator: true },
+      },
+    }),
+  ],
 });
