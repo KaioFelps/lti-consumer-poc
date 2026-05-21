7ce9b5c10404b130cad9ce9798ef991e063f2c31
Kaio Felps <kaiofelipe2005@gmail.com>
2026-05-20 22:46:18 -0300

    test: get (the only one) e2e test passing

---
+++ test/index.ts
@@ -0,0 +1,55 @@
+import fs from "node:fs";
+import { join } from "node:path";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { Test, TestingModule } from "@nestjs/testing";
+import { RedisStore } from "connect-redis";
+import cookieParser from "cookie-parser";
+import { middleware as ejsLayoutsMiddleware } from "express-ejs-layouts";
+import session from "express-session";
+import { AppModule } from "@/app.module";
+import { EnvironmentVars } from "@/config/environment-vars";
+import { Redis } from "@/external/data-store/redis/client";
+
+export async function getTestingApp() {
+  const moduleFixture: TestingModule = await Test.createTestingModule({
+    imports: [AppModule],
+  }).compile();
+
+  const app = moduleFixture.createNestApplication<NestExpressApplication>({
+    httpsOptions: {
+      key: fs.readFileSync(join(process.cwd(), "certs", "localhost-key.pem")),
+      cert: fs.readFileSync(join(process.cwd(), "certs", "localhost.pem")),
+    },
+  });
+
+  const redis = app.get(Redis);
+  const env = app.get(EnvironmentVars);
+
+  app.enableShutdownHooks();
+  app.use(cookieParser());
+  app.use(
+    session({
+      name: "lti_consumer_poc_session",
+      secret: env.app.secret,
+      resave: false,
+      saveUninitialized: false,
+      store: new RedisStore({ client: redis.client }),
+      cookie: {
+        secure: false,
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24, // 24 hours
+      },
+    }),
+  );
+
+  app.useStaticAssets(join(process.cwd(), "public"));
+  app.setBaseViewsDir(join(process.cwd(), "views"));
+  app.setViewEngine("ejs");
+  app.set("view options", {
+    async: true,
+  });
+
+  app.use(ejsLayoutsMiddleware);
+
+  return app;
+}
