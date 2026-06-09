7e68dc722ce501281315b2122927e337149553e2
Kaio Felps <kaiofelipe2005@gmail.com>
2026-06-09 20:53:52 -0300

    fix(`LtiToolsJwtStrategy`): validate authorization token against provider's managed access token rather than trying to read it as a simple JWT

--- src/modules/auth/protected-routes/protected-routes.module.ts
+++ src/modules/auth/protected-routes/protected-routes.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
 import { APP_GUARD } from "@nestjs/core";
+import { OIDCModule } from "@/modules/oidc/oidc.module";
 import { EncryptionModule } from "../encryption/encryption.module";
 import { AuthGuard } from "./guard";
 import { LtiToolsJwtStrategy } from "./strategies/lti-tools-jwt.strategy";
 import { SessionStrategy } from "./strategies/session.strategy";
 
 @Module({
-  imports: [EncryptionModule],
+  imports: [EncryptionModule, OIDCModule],
   providers: [
     SessionStrategy,
     LtiToolsJwtStrategy,
