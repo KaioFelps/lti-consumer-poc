38f5285d42bcd1fda7f290e9873c2ad26431cec7
Kaio Felps <kaiofelipe2005@gmail.com>
2026-06-17 02:24:17 -0300

    test: implement success path test & refactor test suit to use newly added factories

--- src/modules/lti/ags/controllers/lti-line-items-create-line-item.e2e-spec.ts
+++ src/modules/lti/ags/controllers/lti-line-items-create-line-item.e2e-spec.ts
@@ -1,20 +1,20 @@
-import { faker } from "@faker-js/faker";
 import { INestApplication } from "@nestjs/common";
 import request from "supertest";
 import { App } from "supertest/types";
 import { getTestingApp } from "test";
+import courseContextFactory from "test/factories/course-context.factory";
+import deploymentFactory from "test/factories/deployment.factory";
+import externalLtiResourceFactory from "test/factories/external-lti-resource.factory";
+import ltiAssignmentFactory from "test/factories/lti-assignment.factory";
+import ltiResourceLinkFactory from "test/factories/lti-resource-link.factory";
+import ltiToolFactory from "test/factories/lti-tool.factory";
+import personFactory from "test/factories/person.factory";
 import { getToolAndItsOidcAccessToken } from "test/fixtures/oidc";
-import { CoursesRepository } from "@/modules/assignments-and-grades/repositories/courses.repository";
-import { Course } from "@/modules/courses-and-enrollments/entities/course.entity";
+import { DrizzleClient } from "@/external/data-store/drizzle/client";
+import { AssignmentKind } from "@/modules/assignments-and-grades/enums/assignment-kind";
 import { Instructor } from "@/modules/courses-and-enrollments/entities/instructor.entity";
-import { PeopleRepository } from "@/modules/identity/person/people.repository";
-import { Person } from "@/modules/identity/person/person.entity";
-import { CPF } from "@/modules/identity/person/value-objects/cpf";
-import { SystemRole } from "@/modules/identity/user/enums/system-role";
 import { Routes } from "@/routes";
-import { mountContextId } from "../../advantage/context";
-import { LtiToolsRepository } from "../../tools/lti-tools.repository";
-import { ContextConcreteType } from "../enums/context-concrete-type";
+import { AssignmentAndGradeServiceScopes } from "$/assignment-and-grade/scopes";
 
 /**
  * we're not covering points related to the server because the service got its own tests
@@ -24,36 +24,26 @@ import { ContextConcreteType } from "../enums/context-concrete-type";
  */
 
 describe("[e2e::LTI] Create Line Item", async () => {
+  const VALID_CONTENT_TYPE = "application/vnd.ims.lis.v2.lineitem+json";
+
   let app: INestApplication<App>;
-  let toolsRepo: LtiToolsRepository;
-  let peopleRepo: PeopleRepository;
-  let coursesRepo: CoursesRepository;
-  let accessToken: string;
+  let drizzle: DrizzleClient;
 
   beforeAll(async () => {
     app = await getTestingApp();
-    // TODO:
-    // criar uma tool mockada e adicionar diretmaente o client metadata
-    // porque assim podemos passar a jwk diretamente, evitando que o oidc provider
-    // tente fazer uma requisição pra tool mockada — que não existe e, logo, nem
-    // tem como responder a requisição...
-    toolsRepo = app.get(LtiToolsRepository);
-    peopleRepo = app.get(PeopleRepository);
-    coursesRepo = app.get(CoursesRepository);
+    drizzle = app.get(DrizzleClient);
     await app.init();
   });
 
-  beforeEach(async () => {
-    const result = await getToolAndItsOidcAccessToken(app);
-    accessToken = result.accessToken;
-    await toolsRepo.upsertTool(result.tool);
-  });
+  beforeEach(async () => {});
 
   afterAll(async () => {
     await app.close();
   });
 
   it("should require a valid composed context id", async () => {
+    const { tool } = await getValidItems();
+    const { accessToken } = await getToolAndItsOidcAccessToken(app, tool);
     const response = await request(app.getHttpServer())
       .post("/lti/ags/some-invalid-context-id/lineitems")
       .set("authorization", `Bearer ${accessToken}`)
@@ -64,41 +54,15 @@ describe("[e2e::LTI] Create Line Item", async () => {
       });
 
     expect(response.status).toBe(400);
-    console.log(response.body);
     expect(response.body["error"]).toEqual(expect.stringContaining("contexto"));
   });
 
-  const validContentTypes = ["application/vnd.ims.lis.v2.lineitem+json"];
-
   it("should require valid 'content-type' header", async () => {
-    const { accessToken, tool } = await getToolAndItsOidcAccessToken(app);
-    const instructor = Instructor.createUnchecked({
-      person: Person.create({
-        birthDate: new Date(),
-        cpf: CPF.createUnchecked("11122233345"),
-        email: faker.internet.email(),
-        firstName: faker.person.firstName(),
-        surname: faker.person.lastName(),
-        passwordHash: "123456",
-        username: faker.internet.username(),
-        gender: undefined,
-        profilePictureUrl: undefined,
-        systemRole: SystemRole.Admin,
-      }),
-    });
+    const { tool, courseContext } = await getValidItems();
 
-    const course = Course.create({
-      title: "foo",
-      instructorId: instructor.getId(),
-    });
+    const { accessToken } = await getToolAndItsOidcAccessToken(app, tool);
 
-    await toolsRepo.upsertTool(tool);
-    await peopleRepo.createPerson(instructor.getPerson());
-    await coursesRepo.save(course);
-
-    const endpoint = Routes.lti.ags.lineitems.container(
-      mountContextId(course.getId().toString(), ContextConcreteType.Course),
-    );
+    const endpoint = Routes.lti.ags.lineitems.container(courseContext.id);
 
     const response = await request(app.getHttpServer())
       .post(endpoint)
@@ -109,19 +73,85 @@ describe("[e2e::LTI] Create Line Item", async () => {
         label: "teste",
       });
 
-    console.log(response.body);
     expect(response.status).toBe(415);
   });
 
-  it("should allow custom parameters in the request body as per spec", async () => {});
+  it.skip("should allow custom parameters in the request body as per spec", async () => {});
 
-  it("should support the specified body for creating a line item (as per spec)", async () => {});
+  it.skip("should support the specified body for creating a line item (as per spec)", async () => {});
 
-  it("should create a line item when everything is conformant", async () => {});
+  it("should create a line item when everything is conformant", async () => {
+    const { tool, courseContext } = await getValidItems();
+    const { accessToken } = await getToolAndItsOidcAccessToken(app, tool);
 
-  it("should not create another line item when there is already one with given 'resourceId' and 'tag'", async () => {});
+    const endpoint = Routes.lti.ags.lineitems.container(courseContext.id);
 
-  it("should correctly find the course context", async () => {});
+    const response = await request(app.getHttpServer())
+      .post(endpoint)
+      .set("authorization", `Bearer ${accessToken}`)
+      .set("content-type", VALID_CONTENT_TYPE)
+      .send({
+        scoreMaximum: 100,
+        label: "teste",
+      });
 
-  test("the returned line item 'id' should be the endpoint to access that line item", async () => {});
+    expect(response.status).toBe(201);
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        label: expect.any(String),
+        scoreMaximum: expect.any(Number),
+        startDateTime: null,
+        endDateTime: null,
+      }),
+    );
+  });
+
+  it.skip("should not create another line item when there is already one with given 'resourceId' and 'tag'", async () => {});
+
+  it.skip("should correctly find the course context", async () => {});
+
+  test.skip("the returned line item 'id' should be the endpoint to access that line item", async () => {});
+
+  const getValidItems = async () => {
+    const person = await personFactory.createAndPersist(drizzle);
+    const instructor = Instructor.createUnchecked({ person });
+
+    const { course, courseContext } = await courseContextFactory.createAndPersist(drizzle, {
+      instructor,
+    });
+
+    // lti stuff
+    const tool = await ltiToolFactory.createAndPersist(drizzle, {
+      scopes: [AssignmentAndGradeServiceScopes.Lineitem],
+    });
+
+    const deployment = await deploymentFactory.createAndPersist(drizzle, {
+      context: courseContext,
+      tool,
+    });
+
+    // every assignment connects to lti through a resource link
+    const assignmentsResourceLink = await ltiResourceLinkFactory.createAndPersist(drizzle, {
+      tool,
+      deployment,
+      context: courseContext,
+    });
+
+    // platform specific assignment
+    const assignment = await ltiAssignmentFactory.createAndPersist(drizzle, {
+      course,
+      kind: AssignmentKind.ExternalLti,
+      assignmentsResourceLink,
+    });
+
+    // assignment relating local assignment to tool's resource
+    const resource = await externalLtiResourceFactory.createAndPersist(drizzle, {
+      tool,
+      assignment,
+      context: courseContext,
+    });
+
+    return { assignmentsResourceLink, assignment, courseContext, resource, tool, course };
+  };
 });
